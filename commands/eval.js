de3c54a0defbc649b71de6fc81de80e9ec5e6a34
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-12-28 02:01:48 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -8,14 +8,13 @@ module.exports = {
         if (args.length == 0) return message.channel.send('Você esqueceu de colocar o código!');
 
         let code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
-        let value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``
+        let value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.token, () => '*'.repeat(process.env.token.length));
         let embed = new Discord.RichEmbed()
             .setColor('#36393F')
-        
+
         try {
             let resultEval = eval(code)
-            let toEval = typeof resultEval === 'string' ? resultEval : Util.inspect(resultEval,  { depth: 1 })  
-
+            let toEval = typeof resultEval === 'string' ? resultEval : Util.inspect(resultEval,  { depth: 1 }) 
             embed.addField('Resultado', value('js', toEval))
             embed.addField('Tipo', value('css', typeof resultEval))
         } catch (error) {
