5042e749fce872764bb5082295be178f6243a03a
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-12-28 16:02:00 +0000

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,10 +2,9 @@ const Discord = require('discord.js');
 const Util = require('util');
 module.exports = {
     run: async function(message, client, args) {
+        if (!process.env.DEVS.includes(message.author.id)) return message.reply('Only my developers have permission to use this command.');
 
-        if (!process.env.DEVS.includes(message.author.id)) return message.channel.send('Você não tem permissão para usar esse comando!');
-
-        if (args.length == 0) return message.channel.send('Você esqueceu de colocar o código!');
+        if (args.length == 0) return message.reply('You didn\'t provide the code!');
 
         let code = args.join(' ').replace(/^```(js|javascript ?\n)?|```$/g, '')
         let value = (l, c) => `\`\`\`${l}\n${String(c).slice(0, 1000) + (c.length >= 1000 ? '...' : '')}\n\`\`\``.replace(process.env.BOT_TOKEN, () => '*'.repeat(process.env.BOT_TOKEN.length));
@@ -15,8 +14,8 @@ module.exports = {
         try {
             let resultEval = eval(code)
             let toEval = typeof resultEval === 'string' ? resultEval : Util.inspect(resultEval,  { depth: 1 }) 
-            embed.addField('Resultado', value('js', toEval))
-            embed.addField('Tipo', value('css', typeof resultEval))
+            embed.addField('Result', value('js', toEval))
+            embed.addField('Type', value('css', typeof resultEval))
         } catch (error) {
             embed.addField('Error', value('js', error))
         } finally {
