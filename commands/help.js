f862dd953df7722204386e1a5de2351ba17096f0
Almeeida <rodrigofigueire19@gmail.com>
2018-12-28 03:32:35 +0000

    test

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,7 @@ module.exports = {
             .setFooter(`Executed by: ${message.author.tag}`, message.author.displayAvatarURL)
 
         if (args.length == 0) {
-            embed.addField('Como eu funciono?', `Simplicity é um client com focus em moderação, utilidade, musica, e muito mais!\nO meu prefixo é: ${process.env.prefix}!\nPara usar os meus comandos, digite **${process.env.prefix}help <modulo>**.`)
+            embed.addField('Como eu funciono?', `Simplicity é um client com focus em moderação, utilidade, musica, e muito mais!\nO meu prefixo é: ${process.env.PREFIX}!\nPara usar os meus comandos, digite **${process.env.PREFIX}help <modulo>**.`)
             embed.addField('Modulos Existentes:', 'Nenhum por agora ')
             embed.setThumbnail(client.user.displayAvatarURL)
         } else if (args[0].toLowerCase() == ('bot' || 'simplicity')) {
