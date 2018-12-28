e62b5073a95c50022539f7cb80e9bf04bef4c2cc
Almeeida <42935195+Almeeida@users.noreply.github.com>
2018-12-28 16:00:04 +0000

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,25 +1,28 @@
 module.exports = {
     run: async function (message, client, [amount]) {
         
-        if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-            message.reply('Você não tem permissão para executar este comando!')
+        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+            message.reply('You require the **MANAGE_MESSAGES** permission in order to execute this command.')
             return;
-        };
         
-        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) {
-            message.reply('Eu não tenho permissões suficientes para executar esse comando!');
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) 
+            message.reply('I require the **MANAGE_MESSAGES** permission in order to execute this command.');
             return;
-        };
         
         let total = parseInt(amount, 10);
-        if (!total || total < 2 || total > 100) {
-            message.reply('Por favor, indique entre 2 a 100 mensagens!');
+        if (!total || total < 2 || total > 100) 
+            message.reply('Please, give a number between 2 and 100!');
             return;
-        };
     
         const res = await message.channel.fetchMessages({limit: total});
-        message.channel.bulkDelete(res);
-        message.channel.send(`Foram apagadas ${total} mensagens por ${message.author}. `).then(m => m.delete(15000));    
+        message.channel.bulkDelete(res)
+            .catch(err => {
+                message.channel.send('There was an error while trying to delete the messages.');
+                return;
+            });
+        message.channel.send(`${res.size} messages were deleted by ${message.author}.`)
+            .then(m => m.delete(15000))
+            .catch(err => console.log(err)) 
     },
     aliases: ["purge"]
 };
