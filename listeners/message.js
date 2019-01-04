984c8acc09654331fcdf331dda9070aaf9183bc5
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-01-04 10:17:36 +0000

    Update message.js

--- listeners/message.js
+++ listeners/message.js
@@ -1,6 +1,6 @@
 module.exports = function onMessage (message) {
   if (message.author.bot || message.type === 'dm') return
-  if (message.content.includes(`${this.user.id}`)) return message.channel.send(`<a:sockPONGGGG:513111387871117323>`)
+  if (message.mentions.has(this.user.id)) message.channel.send('<a:sockPONGGGG:513111387871117323>')
   if (!message.content.startsWith(process.env.PREFIX)) return
 
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g)
