5568d6a34df6d82ad4f97b5bcc2cfbcb61e50608
Tsugami <yslan3000@gmail.com>
2019-01-07 21:31:36 -0200

    ignorando msg de canais q nao pode enviar

--- listeners/message.js
+++ listeners/message.js
@@ -1,7 +1,9 @@
 module.exports = function onMessage (message) {
-  if (message.author.bot || message.type === 'dm') return
+  if (message.author.bot ||
+    message.type === 'dm' ||
+    !message.guild.me.permissions.has('SEND_MESSAGES') ||
+    !message.content.startsWith(process.env.PREFIX)) return
   if (message.mentions.has(this.user.id)) message.channel.send('<a:sockPONGGGG:513111387871117323>')
-  if (!message.content.startsWith(process.env.PREFIX)) return
   const args = message.content.slice(process.env.PREFIX.length).trim().split(/ +/g)
   const commandName = args.shift().toLowerCase()
   const command = this.commands.find((c, i) => i === commandName || (Array.isArray(c.aliases) && c.aliases.includes(commandName)))
