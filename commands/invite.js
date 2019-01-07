920a41e87ef69327981acedef5514628e0d52984
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-01-07 11:52:53 +0000

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,8 +1,18 @@
 const { MessageEmbed } = require('discord.js')
-module.exports = {
-  run: async function (message, client, args) {
+const Command = require('../structures/Command')
+
+class Invite extends Command {
+  constructor (name, client) {
+    super(name, client)
+    this.aliases = ['inv']
+    this.description = 'This command shows how many persons you\'ve invited to the server.'
+    this.usage = `Usage: **${process.env.PREFIX}inv <mention/id>**`
+    this.category = 'Server'
+    this.argsRequired = false
+  }
+  run (message, args) {
     var user = message.mentions.users.first() || message.guild.members.get([args[0]]) || message.author
-    var targetInvites = await message.guild.fetchInvites()
+    var targetInvites = await message.guild.fetchInvites() // falta await aqui porem nsei onde coloco o async :ThinkPepe:
     var invitesUses = 0
     targetInvites.forEach(invite => {
       if (invite.inviter.id === user.id) {
@@ -17,3 +27,5 @@ module.exports = {
     message.channel.send(embed)
   }
 }
+
+module.exports = Invite
