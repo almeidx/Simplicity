0a33c863dbc3b52aaf438505f6da5f80e13c58fb
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-01-12 23:26:32 +0000

    Delete ping.js

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  run: async function (message, client) {
-    message.channel.send('Ping...')
-      .then(msg => {
-        msg.edit(`Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.floor(client.ping)}ms.`)
-      })
-  },
-  aliases: ['pong']
-}
