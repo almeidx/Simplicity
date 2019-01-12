f010981fa58457fbee6be1751b6c918b94305d42
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-01-12 23:33:21 +0000

    Typo

--- src/listeners/message.js
+++ src/listeners/message.js
@@ -3,8 +3,8 @@ const { CommandContext } = require('../')
 module.exports = async function onMessage (message) {
   if (message.author.bot || message.type === 'dm' || !message.guild.me.permissions.has('SEND_MESSAGES')) return
 
-  if (message.mentions.has(this.user.id) && message.guild.me.permissions.has(['USE_EXTERNAL_EMOJIS', 'ADD_REACTIONS']) && this.emojis.has(process.env.EMOJI_PINGSHOCK_ID)) {
-    message.react(process.env.EMOJI_PINGSHOCK_ID)
+  if (message.mentions.has(this.user.id) && message.guild.me.permissions.has(['USE_EXTERNAL_EMOJIS', 'ADD_REACTIONS']) && this.emojis.has(process.env.EMOJI_PINGSOCK_ID)) {
+    message.react(process.env.EMOJI_PINGSOCK_ID)
   }
 
   const guildData = await this.database.guilds.get(message.guild.id)
