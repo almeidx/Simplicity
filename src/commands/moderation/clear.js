f68e6cb21f02897692dba47a89ee339e17771803
Tsugami <yslan3000@gmail.com>
2019-01-12 16:26:52 -0200

    fixed up clear :full_moon_with_face:

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -17,13 +17,12 @@ class Clear extends Command {
     if (!total || total <= 2 || total >= 100) {
       return message.reply('Por favor, indique entre 2 a 100 mensagens!')
     };
-    const res = await message.channel.fetchMessages({ limit: total })
+    const res = await message.channel.messages.fetch({ limit: total })
     message.channel.bulkDelete(res)
       .catch(() => {
         return message.reply('An error has ocurred while trying to delete the messages.')
       })
     message.channel.send(`${res.size} messages have been deleted by ${message.author}. `)
-      .then(m => m.delete(15000))
       .catch(err => console.log(err))
   }
 }
