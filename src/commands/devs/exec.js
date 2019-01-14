734e747bbd5ef846a5332a8f3ff6bc7efdcc52bd
Almeeida <rodrigofigueire19@gmail.com>
2019-01-14 13:06:37 +0000

    Finished exec.js

--- src/commands/devs/exec.js
+++ src/commands/devs/exec.js
@@ -11,7 +11,12 @@ class Exec extends Command {
     this.argsRequired = true
   }
   run (message, args) {
-    exec() // maybe later i'll finish this
+    function exe (execc, message) {
+      exec(execc)
+        .then(c => message.channel.send(JSON.stringify(c.stdout).replace(/\\n/g, '\n').slice(1, -1)))
+        .catch(console.log)
+    }
+    exe(args.join(' '), message)
   }
 }
 
