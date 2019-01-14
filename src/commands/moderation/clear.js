e95fe0a33c4c9c2befa7ebdc88e353b814e39615
Almeeida <rodrigofigueire19@gmail.com>
2019-01-14 20:54:17 +0000

    Added more locales

---
+++ src/commands/moderation/clear.js
@@ -0,0 +1,29 @@
+const { Command } = require('../../')
+
+class Clear extends Command {
+  constructor (client) {
+    super(client)
+    this.aliases = ['purge', 'prune', 'clean']
+    this.description = 'This command clears messages from the chat.'
+    this.usage = `Usage: **${process.env.PREFIX}clear [amount]**`
+    this.category = 'Moderation'
+    this.argsRequired = true
+    this.permissions = ['MANAGE_MESSAGES']
+    this.clientPermissions = ['MANAGE_MESSAGES']
+  }
+  async run (message, [amount]) {
+    let total = parseInt(amount, 10)
+    if (!total || total <= 2 || total >= 100) {
+      return message.reply('Por favor, indique entre 2 a 100 mensagens!')
+    };
+    const res = await message.channel.messages.fetch({ limit: total })
+    message.channel.bulkDelete(res, true)
+      .catch(() => {
+        return message.reply('An error has ocurred while trying to delete the messages.')
+      })
+    message.channel.send(`${res.size} messages have been deleted by ${message.author}. `)
+      .catch(err => console.log(err))
+  }
+}
+
+module.exports = Clear
---
+++ src/commands/moderation/unban.js
@@ -0,0 +1,47 @@
+const { MessageEmbed } = require('discord.js')
+const { Command } = require('../../')
+
+class Unban extends Command {
+  constructor (client) {
+    super(client)
+    this.aliases = ['ub']
+    this.description = 'This command lets you unban members from your server.'
+    this.usage = `Usage: **${process.env.PREFIX}unban [id] <reason>**`
+    this.category = 'Moderation'
+    this.argsRequired = true
+    this.permissions = ['BAN_MEMBERS']
+    this.clientPermissions = ['BAN_MEMBERS']
+  }
+  run (message, args) {
+    const embed = new MessageEmbed()
+      .setAuthor(message.author.username, message.author.displayAvatarURL({ size: 2048 }))
+      .setTimestamp()
+      .setFooter(`Executed by: ${message.author.tag}`, message.author.displayAvatarURL({ size: 2048 }))
+      .setColor('RED')
+    let reason = args.slice(1)
+    let title = 'Something wen\'t wrong!'
+    let msg
+    if (args[0].match('(^[0-9]*$)')) {
+      try {
+        this.client.users.fetch(args[0]).then(u => {
+          message.guild.members.unban(args[0], { reason: (reason ? message.author.tag + ' | ' + reason : message.author.tag + ' | No reason provided.') })
+          title = 'Member Unbanned'
+          msg = `${u.user.tag} has been unbanned from the server!`
+          embed.addField('Unbanned by:', message.author, true)
+            .addField('Reason:', reason || 'No reason provided.')
+            .setColor('0494bc')
+            .setThumbnail(u.user.displayAvatarURL({ size: 2048 }))
+        })
+      } catch (err) {
+        msg = 'I was unable to find a user with that ID!'
+      }
+    } else {
+      msg = 'You have provided a wrong ID!'
+    }
+    embed.setTitle(title)
+      .setDescription(msg)
+    message.channel.send(embed)
+  }
+}
+
+module.exports = Unban
