6b53306019e6024c436ff45955ca5841be2580ff
Tsugami <yslan3000@gmail.com>
2019-01-14 17:35:17 -0200

    to puto fodase

--- src/commands/devs/exec.js
+++ src/commands/devs/exec.js
@@ -5,12 +5,10 @@ class Exec extends Command {
   constructor (client) {
     super(client)
     this.aliases = ['execute', 'run', 'npm']
-    this.description = 'This command lets my developers execute bash codes.'
-    this.usage = `Usage: **${process.env.PREFIX}exec [code]**`
-    this.category = 'Developer'
-    this.argsRequired = true
+    this.category = 'dev'
+    this.requirements = { argsRequired: true, ownerOnly: true }
   }
-  run (message, args) {
+  run ({ message, args }) {
     function exe (execc, message) {
       exec(execc)
         .then(c => message.channel.send(JSON.stringify(c.stdout).replace(/\\n/g, '\n').slice(1, -1)))
