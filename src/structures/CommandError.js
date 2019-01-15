5133031e3851ef636faf932925db90a1a825fbe2
Tsugami <yslan3000@gmail.com>
2019-01-15 00:43:22 -0200

    organização :+1:

--- src/structures/CommandError.js
+++
@@ -1,9 +0,0 @@
-class CommandError extends Error {
-  constructor (messageError, options) {
-    super(messageError)
-    this.messageError = messageError
-    this.options = options
-  }
-}
-
-module.exports = CommandError
---
+++ src/structures/command/CommandError.js
@@ -0,0 +1,8 @@
+class CommandError extends Error {
+  constructor (message, options) {
+    super(message)
+    this.options = options
+  }
+}
+
+module.exports = CommandError
