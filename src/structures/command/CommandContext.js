c8c603a54db25589e0b8f45749ad8bdb4c849d65
Tsugami <yslan3000@gmail.com>
2019-01-17 14:45:10 -0200

    new method of sending message

--- src/structures/command/CommandContext.js
+++ src/structures/command/CommandContext.js
@@ -1,4 +1,6 @@
 const Constants = require('../../utils/Constants')
+const { MessageEmbed } = require('discord.js')
+
 class CommandContext {
   constructor (options) {
     this.message = options.message
@@ -12,8 +14,11 @@ class CommandContext {
     this.query = options.query
     this.args = options.args
     this.t = options.message.client.i18next.getFixedT(options.language)
+    this.emoji = this._emoji.bind(this)
+    this.send = this._send.bind(this)
   }
-  emoji (name = 'QUESTION', id = false) {
+
+  _emoji (name = 'QUESTION', id = false) {
     name = name.toUpperCase()
     let result
     if (this.guild && this.guild.me.permissions.has('USE_EXTERNAL_EMOJIS') && Constants.EMOJIS_CUSTOM && Constants.EMOJIS_CUSTOM[name]) {
@@ -23,6 +28,34 @@ class CommandContext {
       }
     }
     return result || Constants.EMOJIS[name] || '❓'
+  }
+
+  _send (embed, options, optionsMessage = {}) {
+    options = Object.assign({ error: false, convertText: true, authorFooter: true }, options)
+    if (embed instanceof MessageEmbed) {
+      if (embed.title) embed.title = this.t(embed.title)
+      if (embed.description) embed.description = this.t(embed.description)
+
+      if (embed.fields !== 0) embed.fields = embed.fields.map((f) => ({ name: this.t(f.name), value: this.t(f.value), inline: f.inline }))
+      if (embed.author) embed.author.name = this.t(embed.author.name)
+
+      embed.setTimestamp()
+
+      if (!embed.color) embed.setColor(this.error ? 'RED' : (process.env.COLOR || 'GREEN'))
+      if (options.authorFooter || !embed.footer || embed.footer.text === '') embed.setFooter(this.author.tag, this.author.displayAvatarURL())
+
+      if (this.convertText && !this.channel.permissionsFor(this.guild.me).has('EMBED_LINKS')) {
+        let message = []
+        if (embed.title) message.push(`**${embed.title}**`)
+        if (embed.description) message.description(embed.description)
+        if (embed.fields.length !== 0) embed.fields.forEach(e => message.push(`**${e.name}\n${e.value}`))
+
+        return this.channel.send(message, optionsMessage)
+      }
+      optionsMessage.embed = embed
+      return this.channel.send(optionsMessage)
+    }
+    return this.channel.send(embed)
   }
 }
 
