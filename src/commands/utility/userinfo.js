e631c1849b65a49100bb64c3ebc484246d0f5445
Almeeida <rodrigofigueire19@gmail.com>
2019-01-23 23:49:44 +0000

    Update userinfo.js

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -12,8 +12,8 @@ class UserInfo extends Command {
     const embed = new MessageEmbed()
     if (args.length === 0) {
       embed.addField('commands:userinfo.name', author.tag, true)
-        .addField('commands:userinfo.status', `${emoji(author.presence.status.toUpperCase())} ${t('utils:status.' + author.presence.status)}`)
         .addField('commands:userinfo.id', member.id)
+        .addField('commands:userinfo.status', `${emoji(author.presence.status.toUpperCase())} ${t('utils:status.' + author.presence.status)}`)
         .addField('commands:userinfo.createdAt', `${moment(author.createdAt).format('LL')} (\`${moment(author.createdAt).fromNow()}\`)`)
         .addField('commands:userinfo.joinedAt', `${moment(member.joinedAt).format('LL')} (\`${moment(member.joinedAt).fromNow()}\`)`)
         .setAuthor(author.tag, author.displayAvatarURL({ size: 2048 }))
@@ -22,8 +22,8 @@ class UserInfo extends Command {
     } else if (message.mentions.members.first()) {
       const mem = message.mentions.members.first()
       embed.addField(t('commands:userinfo.name'), mem.user.tag, true)
+	    .addField(t('commands:userinfo.id'), mem.id)
         .addField(t('commands:userinfo.status'), `${emoji(mem.presence.status.toUpperCase())} ${t('utils:status.' + mem.presence.status)}`, true)
-        .addField(t('commands:userinfo.id'), mem.id)
         .addField(t('commands:userinfo.createdAt'), `${moment(mem.user.createdAt).format('LL')} (\`${moment(mem.user.createdAt).fromNow()}\`)`, true)
         .addField(t('commands:userinfo.joinedAt'), `${moment(guild.members.get(mem.id).joinedAt).format('LL')} (\`${moment(guild.members.get(mem.id).joinedAt).fromNow()}\`)`, true)
         .setAuthor(mem.user.tag, mem.user.displayAvatarURL({ size: 2048 }))
@@ -33,8 +33,8 @@ class UserInfo extends Command {
       this.client.users.fetch(args)
         .then(u => {
           embed.addField(t('commands:userinfo.name'), u.tag, true)
+		    .addField(t('commands:userinfo.id'), u.id, true)
             .addField(t('commands:userinfo.status'), `${emoji(u.presence.status.toUpperCase())} ${t('utils:status.' + u.presence.status)}`)
-            .addField(t('commands:userinfo.id'), u.id, true)
             .addField(t('commands:userinfo.createdAt'), `${moment(u.createdAt).format('LL')} (\`${moment(u.createdAt).fromNow()}\`)`)
             .setThumbnail(u.displayAvatarURL({ size: 2048 }))
             .setAuthor(u.tag, u.displayAvatarURL({ size: 2048 }))
