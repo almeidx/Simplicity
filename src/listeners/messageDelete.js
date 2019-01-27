31c8629a92f4f52df12dbb99f97eab626c15e822
Almeeida <rodrigofigueire19@gmail.com>
2019-01-27 21:34:21 +0000

    Cleaning messageDelete

--- src/listeners/messageDelete.js
+++ src/listeners/messageDelete.js
@@ -3,22 +3,20 @@ module.exports = async function messageDelete (message) {
   const chan = message.guild.channels.find(ch => ch.name === 'logs')
   if (message.channel === chan) return
   if (chan) {
+    const embed = new MessageEmbed()
+      .addField('Content', (message.content.slice(0, 1020) + message.content.length >= 1024 ? ' ...' : message.content) || ' ')
+      .setDescription(`**Message sent by ${message.author} deleted in ${message.channel}**`)
+      .setAuthor(message.author.tag, message.author.displayAvatarURL({ size: 2048 }))
+      .setFooter(`ID: ${message.author.id}`, message.author.displayAvatarURL({ size: 2048 }))
+      .setColor(process.env.COLOR)
+      .setTimestamp()
     if (message.guild.me.hasPermission('VIEW_AUDIT_LOG')) {
       const entry = await message.guild.fetchAuditLogs({ type: 'MESSAGE_DELETE' }).then(audit => audit.entries.first())
-      let user
       if (entry.extra.channel.id === message.channel.id && entry.target.id === message.author.id && entry.createdTimestamp > Date.now() - 5000) {
-        user = entry.executor
-      } else {
-        user = message.author
+        const user = entry.executor
+        embed.setDescription(`**Message sent by ${message.author} deleted in ${message.channel} by ${user}**`)
       }
-      let embed = new MessageEmbed()
-        .setAuthor(message.author.tag, message.author.displayAvatarURL({ size: 2048 }))
-        .setDescription(`**Message sent by ${message.author} deleted in ${message.channel} by ${user}**`)
-        .addField('Content', (message.content.slice(0, 1020) + message.content.length >= 1024 ? ' ...' : message.content) || 'An error occurred while inputting the message content.')
-        .setFooter(`ID: ${message.author.id}`, message.author.displayAvatarURL({ size: 2048 }))
-        .setTimestamp()
-        .setColor(process.env.COLOR)
-      chan.send(embed)
     }
+    chan.send(embed)
   }
 }
