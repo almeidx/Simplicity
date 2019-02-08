f7249de09b11148fa0198fb6b4c8d5a1f5ddb1ee
Almeeida <rodrigofigueire19@gmail.com>
2019-02-08 01:32:53 +0000

    Cleaning and optimizing

--- src/commands/utility/emoji.js
+++ src/commands/utility/emoji.js
@@ -14,25 +14,26 @@ class Emoji extends Command {
       .setColor('36393f')
     try {
       let custom = args.join(' ').split(RegexEmojis)[0]
+      console.log(custom)
       if (custom) {
         const id = custom.split(':')[ custom.split(':').length - 1 ].replace(/>/g, '')
         const gif = custom.split(':')[0].replace(/</g, '') === 'a'
         embed.setImage(`attachment://Simplicity-Emoji.${gif ? 'gif' : 'png'}`)
           .attachFiles(new MessageAttachment(`https://cdn.discordapp.com/emojis/${id}.${gif ? 'gif' : 'png'}?v=1`, `Simplicity-Emoji.${gif ? 'gif' : 'png'}`))
-        await send(embed)
+        await send(embed, { autoFooter: false, autoAuthor: false, autoTimestamp: false })
           .catch(async () => {
             const url = `https://twemoji.maxcdn.com/2/72x72/${args.join(' ').codePointAt().toString(16)}.png`
             await request.get(url)
             embed.setImage('attachment://Simplicity-Emoji.png')
               .attachFiles(new MessageAttachment(url, 'Simplicity-Emoji.png'))
-            await send(embed)
+            await send(embed, { autoFooter: false, autoAuthor: false, autoTimestamp: false })
               .catch(() => {
                 send(embed, { error: true })
               })
           })
       }
     } catch (err) {
-      send('DEU MERDA PORRA SLA')
+      send(t('errors:general'))
     }
   }
 }
--- src/structures/command/CommandContext.js
+++ src/structures/command/CommandContext.js
@@ -30,57 +30,32 @@ class CommandContext {
     }
     return Constants.EMOJIS[name] || (noEmoji ? '' : '❓')
   }
-
   _send (embed, options, optionsMessage = {}) {
-    options = Object.assign({ error: false, convertText: true, autoFooter: true, autoAuthor: true, options: {} }, options)
+    options = Object.assign({ error: false, convertText: true, autoFooter: true, autoAuthor: true, autoTimestamp: true, options: {} }, options)
     const tOptions = Object.assign({ title: {}, description: {} }, options.options)
-
     if (embed instanceof MessageEmbed) {
-      embed.setTimestamp()
-
       const checkGetT = (text) => text.includes(':') && !text.includes(' ')
       const COLOR = process.env.COLOR ? process.env.COLOR : this.guild.me.displayColor !== 0 ? this.guild.me.displayColor : this.member.displayColor !== 0 ? this.member.displayColor : 'GREEN'
-
-      if (embed.title && checkGetT(embed.title)) {
-        embed.title = this.t(embed.title, tOptions.title)
-      }
-
-      if (embed.description && checkGetT(embed.description)) {
-        embed.description = (options.error ? this.emoji('ERROR') + ' ' : '') + this.t(embed.description, tOptions.description)
-      }
-
+      if (embed.title && checkGetT(embed.title)) embed.title = this.t(embed.title, tOptions.title)
+      if (embed.description && checkGetT(embed.description)) embed.description = (options.error ? this.emoji('ERROR') + ' ' : '') + this.t(embed.description, tOptions.description)
       if (embed.fields !== 0) {
         embed.fields = embed.fields.map((f) => ({
           name: checkGetT(f.name) ? this.t(f.name) : f.name,
           value: checkGetT(f.value) ? this.t(f.value) : f.value,
           inline: f.inline }))
       }
-
-      if (embed.author && embed.author.name && checkGetT(embed.author.name)) {
-        embed.author.name = this.t(embed.author.name)
-      }
-
-      if (!embed.color) {
-        embed.setColor(options.error ? 'RED' : COLOR)
-      }
-
-      if (options.autoFooter) {
-        embed.setFooter(`${this.t('utils:footer')} ${this.author.tag}`, this.author.displayAvatarURL({ size: 2048 }))
-      }
-
-      if (options.autoAuthor) {
-        embed.setAuthor(this.author.username, this.author.displayAvatarURL({ size: 2048 }))
-      }
-
+      if (embed.author && embed.author.name && checkGetT(embed.author.name)) embed.author.name = this.t(embed.author.name)
+      if (!embed.color) embed.setColor(options.error ? 'RED' : COLOR)
+      if (options.autoFooter) embed.setFooter(`${this.t('utils:footer')} ${this.author.tag}`, this.author.displayAvatarURL({ size: 2048 }))
+      if (options.autoAuthor) embed.setAuthor(this.author.username, this.author.displayAvatarURL({ size: 2048 }))
+      if (options.autoTimestamp) embed.setTimestamp()
       if (options.convertText && !this.channel.permissionsFor(this.guild.me).has('EMBED_LINKS')) {
         let message = []
         if (embed.title) message.push(`**${embed.title}**`)
         if (embed.description) message.push(embed.description)
         if (embed.fields.length !== 0) embed.fields.forEach(e => message.push(`**${e.name}\n${e.value}`))
-
         return this.channel.send(message, optionsMessage)
       }
-
       optionsMessage.embed = embed
       return this.channel.send(optionsMessage)
     }
