aefe36ece7a82f0bae78675853bd5f17c63d6ad4
Almeeida <rodrigofigueire19@gmail.com>
2019-02-23 01:46:54 +0000

    Adding proper locales and visually fixing the serverinfo command

--- src/commands/server/serverinfo.js
+++ src/commands/server/serverinfo.js
@@ -8,25 +8,24 @@ class ServerInfo extends Command {
     this.category = 'server'
   }
   run ({ author, guild, channel, t }) {
-    const available = guild.members.filter(user => user.presence.status !== 'offline').size
+    const online = guild.members.filter(user => user.presence.status !== 'offline').size
     const offline = guild.members.filter(user => user.presence.status === 'offline').size
     const bots = guild.members.filter(user => user.user.bot).size
     const members = guild.memberCount
     const channels = guild.channels.filter(ch => ch.type === 'text' || ch.type === 'voice').size
     const emojis = guild.emojis.size
     const roles = guild.roles.size
-    const verificationLevel = ['None', 'Low', 'Medium', '(╯°□°）╯︵ ┻━┻', '┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻'][guild.verificationLevel]
-    const verificationName = ['Unrestricted.', 'Must have a verified email on their Discord account.', 'Must be registered on Discord for longer than 5 minutes.', 'Must be a member of the server for longer than 10 minutes.', 'Must have a verified phone on their Discord account.'][guild.verificationLevel]
-    const embed = new Embed({ t, guild })
+    const verificationLevel = [t('commands:serverinfo.none'), t('commands:serverinfo.low'), t('commands:serverinfo.medium'), '(╯°□°）╯︵ ┻━┻', '┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻'][guild.verificationLevel]
+    const verificationName = [t('commands:serverinfo.unrestricted'), t('commands:serverinfo.levelTwo'), t('commands:serverinfo.levelThree'), t('commands:serverinfo.levelFour'), t('commands:serverinfo.levelFive')][guild.verificationLevel]
+    const embed = new Embed({ t, guild, author })
       .addField('commands:serverinfo.name', guild.name, true)
-      .addField('commands:serverinfo.owner', guild.owner, true)
+      .addField('commands:serverinfo.owner', guild.owner.user.tag, true)
       .addField('commands:serverinfo.id', guild.id, true)
-      .addField('commands:serverinfo.created', moment(guild.createdAt).format('LL'))
-      .addField(t('commands:serverinfo.members', { members }), `**${t('commands:serverinfo.available')}:** ${available}\n**${t('utils:status.offline')}:** ${offline}\n${t('utils:bots')}:** ${bots}`, true)
+      .addField('commands:serverinfo.created', moment(guild.createdAt).format('LLL'), true)
       .addField('commands:serverinfo.channelsEmojisRoles', `${channels} | ${emojis} | ${roles}`, true)
+      .addField('commands:serverinfo.membersOnlineOfflineBots', `${members} | ${online} | ${offline} | ${bots}`, true)
       .addField(`${t('commands:serverinfo.verificationLevel')}: ${verificationLevel}`, verificationName)
-      .setThumbnail(guild.iconURL())
-      .setFooter(`${t('utils:footer')} ${author.tag}`, author.displayAvatarURL({ size: 2048 }))
+      .setThumbnail(guild.iconURL({ size: 2048 }))
     channel.send(embed)
   }
 }
