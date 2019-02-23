77869763812c5ba10ea1dd1a5aa3406508e417ec
Almeeida <rodrigofigueire19@gmail.com>
2019-02-23 02:02:51 +0000

    Improving the names and the code

--- src/commands/server/serverinfo.js
+++ src/commands/server/serverinfo.js
@@ -8,23 +8,21 @@ class ServerInfo extends Command {
     this.category = 'server'
   }
   run ({ author, guild, channel, t }) {
-    const online = guild.members.filter(user => user.presence.status !== 'offline').size
-    const offline = guild.members.filter(user => user.presence.status === 'offline').size
+    const online = guild.members.filter(user => user.presence.status !== 'offline' && !user.user.bot).size
+    const offline = guild.members.filter(user => user.presence.status === 'offline' && !user.user.bot).size
     const bots = guild.members.filter(user => user.user.bot).size
     const members = guild.memberCount
     const channels = guild.channels.filter(ch => ch.type === 'text' || ch.type === 'voice').size
     const emojis = guild.emojis.size
     const roles = guild.roles.size
-    const verificationLevel = [t('commands:serverinfo.none'), t('commands:serverinfo.low'), t('commands:serverinfo.medium'), '(╯°□°）╯︵ ┻━┻', '┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻'][guild.verificationLevel]
-    const verificationName = [t('commands:serverinfo.unrestricted'), t('commands:serverinfo.levelTwo'), t('commands:serverinfo.levelThree'), t('commands:serverinfo.levelFour'), t('commands:serverinfo.levelFive')][guild.verificationLevel]
     const embed = new Embed({ t, guild, author })
       .addField('commands:serverinfo.name', guild.name, true)
       .addField('commands:serverinfo.owner', guild.owner.user.tag, true)
       .addField('commands:serverinfo.id', guild.id, true)
       .addField('commands:serverinfo.created', moment(guild.createdAt).format('LLL'), true)
       .addField('commands:serverinfo.channelsEmojisRoles', `${channels} | ${emojis} | ${roles}`, true)
-      .addField('commands:serverinfo.membersOnlineOfflineBots', `${members} | ${online} | ${offline} | ${bots}`, true)
-      .addField(`${t('commands:serverinfo.verificationLevel')}: ${verificationLevel}`, verificationName)
+      .addField('commands:serverinfo.onlineOfflineBotsTotal', `${online} | ${offline} | ${bots} | ${members}`, true)
+      .addField(`${t('commands:serverinfo.verificationLevel')}: ${t(`commands:serverinfo.verificationName.${guild.verificationLevel}`)}`, t(`commands:serverinfo.verificationDetails.${guild.verificationLevel}`), true)
       .setThumbnail(guild.iconURL({ size: 2048 }))
     channel.send(embed)
   }
