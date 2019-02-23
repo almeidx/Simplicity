8dbc52cf316bb20c17c2e69842a52df58df6f7a6
Tsugami <yslan3000@gmail.com>
2019-02-23 12:10:46 -0300

    add channels

--- src/commands/server/serverinfo.js
+++ src/commands/server/serverinfo.js
@@ -8,11 +8,17 @@ class ServerInfo extends Command {
     this.category = 'server'
   }
   run ({ author, guild, channel, t }) {
+    // Members
     const online = guild.members.filter(user => user.presence.status !== 'offline' && !user.user.bot).size
     const offline = guild.members.filter(user => user.presence.status === 'offline' && !user.user.bot).size
     const bots = guild.members.filter(user => user.user.bot).size
     const members = guild.memberCount
-    const channels = guild.channels.filter(ch => ch.type === 'text' || ch.type === 'voice').size
+
+    // Channels
+    const text = guild.channels.filter(channel => channel.type === 'text').size
+    const voice = guild.channels.filter(channel => channel.type === 'voice').size
+    const category = guild.channels.filter(channel => channel.type === 'category').size
+
     const emojis = guild.emojis.size
     const roles = guild.roles.size
     const embed = new Embed({ t, guild, author })
@@ -20,9 +26,10 @@ class ServerInfo extends Command {
       .addField('commands:serverinfo.owner', guild.owner.user.tag, true)
       .addField('commands:serverinfo.id', guild.id, true)
       .addField('commands:serverinfo.created', moment(guild.createdAt).format('LLL'), true)
-      .addField('commands:serverinfo.channelsEmojisRoles', `${channels} | ${emojis} | ${roles}`, true)
+      .addField('commands:serverinfo.Channels', `${text} | ${voice} | ${category}`, true)
+      .addField('commands:serverinfo.EmojisRoles', `${emojis} | ${roles}`, true)
       .addField('commands:serverinfo.onlineOfflineBotsTotal', `${online} | ${offline} | ${bots} | ${members}`, true)
-      .addField(t('commands:serverinfo.verificationLevel', { level: guild.verificationLevel }), t(`commands:serverinfo.verificationDetails.${guild.verificationLevel}`), true)
+      .addField('commands:serverinfo.verificationLevel', `commands:serverinfo.verificationDetails.${guild.verificationLevel}`, true, { level: guild.verificationLevel })
       .setThumbnail(guild.iconURL({ size: 2048 }))
     channel.send(embed)
   }
