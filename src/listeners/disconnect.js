4666eb0c87943944aefb20a41e37677aca756f13
Almeeida <rodrigofigueire19@gmail.com>
2019-03-05 21:51:21 +0000

    Removed useless code

--- src/listeners/disconnect.js
+++ src/listeners/disconnect.js
@@ -1,16 +1,6 @@
-const { Embed } = require('../')
+const moment = require('moment')
 function Disconnect () {
-  console.log(`The bot has disconnected at ${require('moment')().format('LLL')}`)
-
-  const embed = new Embed()
-    .setTimestamp()
-    .setTitle('Bot disconnected')
-    .setColor('RED')
-    .setFooter(this.user.username, this.user.displayAvatarURL())
-
-  if (process.env.BOT_LOG && this.channels.has(process.env.BOT_LOG)) {
-    this.channels.get(process.env.BOT_LOG).send(embed)
-  }
+  console.log(`The bot has disconnected at ${moment().format('LLL')}`)
 }
 
 module.exports = Disconnect
