6b39e496e4ccdaeb4e019f4ef3f5f8175d43141c
Tsugami <yslan3000@gmail.com>
2019-03-05 13:39:46 -0300

    added message url

--- src/listeners/messageUpdate.js
+++ src/listeners/messageUpdate.js
@@ -1,8 +1,11 @@
 const { Embed, LogUtils, Constants } = require('../')
+const clean = (str) => str.slice(0, 1020) + str.length >= 1024 ? '...' : str
+
 async function messageUpdate (oldMessage, newMessage) {
   const { channel, t } = await LogUtils.getChannel(this, oldMessage.guild, 'JOIN_AND_LEAVE')
 
   if (channel && oldMessage.guild.me.permissions.has('READ_AUDIT_LOGS')) {
+    const url = oldMessage.url
     const user = oldMessage.author
     const msgChannel = oldMessage.channel
     const oldContent = oldMessage.content
@@ -14,25 +17,25 @@ async function messageUpdate (oldMessage, newMessage) {
 
     // MESSAGE EDITED
     if (oldContent !== newContent) {
-      embed.setDescription('loggers:messageEdited', { user, channel: msgChannel })
-        .addField('Before', (oldContent.slice(0, 1020) + oldContent.length >= 1024 ? '...' : oldContent) || 'errors:general', true)
-        .addField('After', (newContent.slice(0, 1020) + newContent.length >= 1024 ? '...' : newContent) || 'errors:general', true)
+      embed.setDescription('loggers:messageEdited', { url, user, channel: msgChannel })
+        .addField('Before', clean(oldContent) || 'errors:general', true)
+        .addField('After', clean(newContent) || 'errors:general', true)
         .setColor(Constants.COLORS.MESSAGE_EDIT)
-      return LogUtils.send(embed)
+      return LogUtils.send(channel, embed)
     } else
     // MESSAGE PINS
     if (oldMessage.pinned !== newMessage.pinned) {
       // MESSAGE UNPINNED
       if (oldMessage.pinned && !newMessage.pinned) {
-        embed.setDescription('loggers:messageUnpinned', { user, channel: msgChannel })
+        embed.setDescription('loggers:messageUnpinned', { url, user, channel: msgChannel })
           .setColor(Constants.COLORS.MESSAGE_UNPIN)
-        return LogUtils.send(embed)
+        return LogUtils.send(channel, embed)
       } else
       // MESSAGE PINNED
       if (!oldMessage.pinned && newMessage.pinned) {
-        embed.setDescription('loggers:messagePinned', { user, channel: msgChannel })
+        embed.setDescription('loggers:messagePinned', { url, user, channel: msgChannel })
           .setColor(Constants.COLORS.MESSAGE_PIN)
-        return LogUtils.send(embed)
+        return LogUtils.send(channel, embed)
       }
     }
   }
