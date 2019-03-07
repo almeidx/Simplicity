53def732f8b980d994d775f7bce7421a7e47fb0e
Tsugami <yslan3000@gmail.com>
2019-03-06 22:54:11 -0300

    rework

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -6,46 +6,27 @@ class Ban extends Command {
     this.aliases = ['bean']
     this.category = 'mod'
     this.parameters = [{
-      type: 'user',
-      required: true
+      type: 'member',
+      required: true,
+      argFirst: true,
+      onlyRoleHighest: true,
+      onlyBotRoleHighest: true,
+      missingError: 'errors:invalidUser'
+    }, {
+      type: 'string',
+      default: 'noReason'
     }]
-    this.requirements = { argsRequired: true, permissions: ['BAN_MEMBERS'], clientPermissions: ['BAN_MEMBERS'] }
+    this.requirements = { permissions: ['BAN_MEMBERS'], clientPermissions: ['BAN_MEMBERS'] }
   }
-  run ({ author, guild, send, member, t, args }, user) {
-    const reason = args.slice(1).join(' ') || 'commands:ban.noReason'
 
-    const embed = new Embed({ t, author, member, guild })
-      .setTimestamp()
-      .setTitle('errors:invalidParameters')
-
-    if (!user) {
-      embed.setDescription('commands:ban.invalidUser')
-        .setError()
-      return send(embed)
-    }
-
-    // AUTHOR ROLES ARE LOWER OR THE SAME AS THE USER
-    if (member.roles.highest.position <= user.roles.highest.position) {
-      embed.setError()
-        .setDescription('errors:userMissingRole', { action: 'commands:ban.action' })
-      return send(embed)
-    } else
-
-    // BOT ROLES ARE LOWER OR THE SAME AS THE USER
-    if (guild.me.roles.highest.position <= user.roles.highest.position) {
-      embed.setError()
-        .setDescription('errors:clientMissingRole', { action: t('commands:ban.action') })
-      return send(embed)
-    } else {
-      user.ban({ days: 7, reason: `${author.tag} | ${reason}` })
-
-      embed.setTitle('commands:ban.success')
-        .setDescription('commands:ban.userBanned', { user })
-        .addField('commands:ban.bannedBy', author, true)
-        .addField('commands:ban.reason', reason, true)
-
-      return send(embed)
-    }
+  async run ({ author, send, t }, member, reason) {
+    await member.ban(reason)
+    const embed = new Embed({ t, author })
+      .setTitle('commands:ban.success')
+      .setDescription('commands:ban.userBanned', { user: member })
+      .addField('commands:ban.bannedBy', `${author}`, true)
+      .addField('commands:ban.reason', reason, true)
+    send(embed)
   }
 }
 
