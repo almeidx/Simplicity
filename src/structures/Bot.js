843183372c3b8c0d7ee3a96a70cc4da865dbee70
Almeeida <rodrigofigueire19@gmail.com>
2019-03-09 19:41:37 +0000

    Fixing Loaders path

--- src/structures/Bot.js
+++ src/structures/Bot.js
@@ -1,5 +1,5 @@
 const { Client, Collection } = require('discord.js')
-const Loaders = require('../loaders')
+const Loaders = require('../')
 const Database = require('../database/Database')
 const fs = require('fs')
 const Path = require('path')
