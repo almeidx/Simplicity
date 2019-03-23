61b26ed9c9c30acdb42f44c025ab8e64d54bdec9
Tsugami <yslan3000@gmail.com>
2019-03-23 14:30:11 -0300

    moving Parameter

--- src/structures/Parameter.js
+++ src/structures/Parameter.js
@@ -1,6 +1,9 @@
 class Parameter {
   constructor (options = {}) {
-    options = Object.assign({ required: true, missingError: 'errors:missingParameters' }, options)
+    options = Object.assign({
+      required: true,
+      missingError: 'errors:missingParameters' },
+    options)
     this.required = !!options.required
     this.missingError = options.missingError
   }
