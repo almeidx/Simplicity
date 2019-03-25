3edec79049fd1fec79d9880809bc2d4ff0a5b721
Tsugami <yslan3000@gmail.com>
2019-03-25 15:00:23 -0300

    fixing bugs in ban command

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -1,15 +1,13 @@
-const { Command, Embed, Parameters: { MemberParameter }, CommandError } = require('../../')
-const Collector = require('../../utils/Collector')
-
+const { Command, Embed, Parameters: { MemberParameter, UserParameter }, CommandError } = require('../../')
 const missingError = 'errors:invalidUser'
-const Member = new MemberParameter({
-  acceptSelf: false,
+const optionsParameter = {
   required: false,
-  argFirst: true,
-  onlyRoleHighest: true,
-  onlyBotRoleHighest: true,
-  missingError
-})
+  acceptSelf: false,
+  checkGlobally: true,
+  errors: {
+    missingError
+  }
+}
 
 class Ban extends Command {
   constructor (client) {
@@ -19,23 +17,22 @@ class Ban extends Command {
     this.requirements = { permissions: ['BAN_MEMBERS'], clientPermissions: ['BAN_MEMBERS'] }
   }
 
-  async run (context) {
-    const { guild, send, author, t, args, emoji, message } = context
-    const query = args[0] || ''
-    let member = await Member.handle(context, query)
-    let id = member && member.id
+  async run ({ args, guild, client, member: memberAuthor, t, send }) {
+    const user = await UserParameter.search(args[0], { client, guild }, optionsParameter)
+    if (!user) throw new CommandError(missingError)
 
-    if (!member) {
-      const user = await this.client.users.fetch(query).catch(() => { throw new CommandError(missingError) })
+    const member = user && guild.member(user)
+
+    if (member) {
+      await MemberParameter.verifyExceptions(member, { guild, memberAuthor, commandName: this.name }, optionsParameter)
+    } else {
       const bans = await guild.fetchBans()
       const alreadyBanned = bans && bans.get(user.id)
 
       if (alreadyBanned) {
-        const message = alreadyBanned.reason ? 'commands:ban.alreadyBannedReason' : 'commands:ban.alreadyBannedNoReason'
-        throw new CommandError(message, { user, reason: (alreadyBanned.reason || '') })
+        const reason = alreadyBanned.reason ? 'commands:ban.alreadyBannedReason' : 'commands:ban.alreadyBannedNoReason'
+        throw new CommandError(reason, { user, reason: alreadyBanned.reason })
       }
-      id = user.id
-      member = user
     }
 
     let days = 0
@@ -43,22 +40,14 @@ class Ban extends Command {
       const number = Number(i.replace(/--(days|d)/i, ''))
       if (!isNaN(number)) days = number
       return ''
-    }) || t('errors:noReason')
+    }) || 'errors:noReason'
 
-    const { type, collector } = Collector.handle(message, {
-      reactions: [emoji('SUCCESS', { id: true }), emoji('CANCEL', { id: true })],
-      text: `Para continua reaja com ${emoji('SUCCESS')} e para cancelar ${emoji('CANCEL')}`,
-      options: { max: 1 }
-    }, {
-
-    })
-
-    // await guild.users.ban(id, { reason, days })
+    await guild.users.ban(user.id, { reason, days })
     return send(
       new Embed({ t })
         .setTitle('commands:ban.success')
         .setDescription('commands:ban.userBanned', { user: member })
-        .addField('commands:ban.bannedBy', `${author}`, true)
+        .addField('commands:ban.bannedBy', `${memberAuthor}`, true)
         .addField('commands:ban.reason', reason, true)
         .setFooter('ba e' + days)
     )
