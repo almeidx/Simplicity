e1172ce849acde81948f4798c2d42c24d75c8c9a
Tsugami <yslan3000@gmail.com>
2019-03-25 16:07:42 -0300

    added collector in ban command

--- src/commands/mod/ban.js
+++ src/commands/mod/ban.js
@@ -14,10 +14,10 @@ class Ban extends Command {
     super(client)
     this.aliases = ['bean']
     this.category = 'mod'
-    this.requirements = { permissions: ['BAN_MEMBERS'], clientPermissions: ['BAN_MEMBERS'] }
+    this.requirements = { permissions: ['BAN_MEMBERS'], clientPermissions: ['BAN_MEMBERS'], guildOnly: true }
   }
 
-  async run ({ args, guild, client, member: memberAuthor, t, send }) {
+  async run ({ args, guild, client, member: memberAuthor, t, send, message }) {
     const user = await UserParameter.search(args[0], { client, guild }, optionsParameter)
     if (!user) throw new CommandError(missingError)
 
@@ -42,15 +42,43 @@ class Ban extends Command {
       return ''
     }) || 'errors:noReason'
 
-    await guild.users.ban(user.id, { reason, days })
-    return send(
-      new Embed({ t })
-        .setTitle('commands:ban.success')
-        .setDescription('commands:ban.userBanned', { user: member })
-        .addField('commands:ban.bannedBy', `${memberAuthor}`, true)
-        .addField('commands:ban.reason', reason, true)
-        .setFooter(!days ? '' : 'commands:ban.days', null, { days })
-    )
+    const embed = new Embed({ t })
+    const msg = await send(embed.setDescription('utils:question', { confirm: 'yes', cancel: 'not' }))
+    const wordsContinue = ['y', 'yes', 'sim', 's', 'continue']
+    const wordsCancel = ['n', 'não', 'not', 'nop', 'cancel']
+    const words = wordsContinue.concat(wordsCancel)
+    const filter = (m) => m.author.id === memberAuthor.user.id && words.includes(m.content.toLowerCase())
+    const collector = await message.channel.createMessageCollector(filter, { time: 15000, max: 1 })
+
+    const deleteMessages = (m) => {
+      if (message.channel.permissionsFor(guild.me).has('MANAGE_MESSAGES')) {
+        if (m) m.delete()
+        msg.delete()
+      }
+    }
+
+    collector.once('collect', async (m) => {
+      const word = m.content.toLowerCase()
+      deleteMessages()
+      if (wordsCancel.includes(word)) {
+        await send(embed.setDescription('commands:ban.banCanceled'))
+      } else {
+        // await guild.users.ban(user.id, { reason, days })
+        return send(
+          new Embed({ t })
+            .setTitle('commands:ban.success')
+            .setDescription('commands:ban.userBanned', { user: member })
+            .addField('commands:ban.bannedBy', `${memberAuthor}`, true)
+            .addField('commands:ban.reason', reason, true)
+            .setFooter(!days ? '' : 'commands:ban.days', null, { days })
+        )
+      }
+    })
+
+    collector.once('end', () => {
+      deleteMessages()
+      send(embed.setDescription('utils:commandCanceledIdle'))
+    })
   }
 }
 
