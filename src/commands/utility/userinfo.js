1dcb36476e027acc1a05a831ed31e3f298cfe970
Almeeida <rodrigofigueire19@gmail.com>
2019-03-27 19:44:13 +0000

    adding more useful info onto userinfo command

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -1,9 +1,5 @@
-const { Command, Embed, Constants: { SPOTIFY_LOGO_PNG }, Parameters: { UserParameter } } = require('../../')
-const { MessageAttachment } = require('discord.js')
+const { Command, Embed, Constants: { SPOTIFY_LOGO_PNG_URL }, Parameters: { UserParameter } } = require('../../')
 const moment = require('moment')
-
-const nameAttachment = 'spotify-logo.png'
-const spotifyAttachment = new MessageAttachment(SPOTIFY_LOGO_PNG, nameAttachment)
 
 class UserInfo extends Command {
   constructor (client) {
@@ -13,16 +9,17 @@ class UserInfo extends Command {
     this.requirements = { clientPermissions: ['EMBED_LINKS'] }
   }
 
-  async run ({ author, channel, client, guild, send, t, query, emoji }) {
-    const user = (!query ? author : await UserParameter.parse(query, { client, guild }, {
+  async run ({ author, channel, guild, client, send, t, query, emoji }) {
+    const user = (!query ? author : await UserParameter.parse(query, {
       errors: { missingError: 'errors:invalidUser' },
       required: true
-    }))
+    }, { client, guild }))
 
     const member = guild && guild.member(user)
 
     const presence = client.users.has(user.id) && user.presence
-    const status = presence && presence.status
+    const clientStatus = presence && presence.clientStatus
+    const status = clientStatus && (clientStatus.desktop || clientStatus.mobile || clientStatus.web)
 
     const created = moment(user.createdAt)
     const joined = member && moment(member.joinedAt)
@@ -30,21 +27,25 @@ class UserInfo extends Command {
     const embed = new Embed({ author, t, emoji, autoAuthor: false })
       .setAuthor(user.tag, user.displayAvatarURL())
       .setThumbnail(user.displayAvatarURL())
-      .addField('commands:userinfo.name', user.tag, true)
-      .addField('commands:userinfo.id', user.id, true)
+      .addField('commands:userinfo.username', user.tag, true)
+      
+    if (member && member.nickname) embed.addField('commands:userinfo.nickname', member.nickname, true)
+    
+    embed.addField('commands:userinfo.id', user.id, true)
 
     if (status) embed.addField('commands:userinfo.status', `#${presence.status} $$utils:status.${presence.status}`, true)
+    if (member && member.roles && member.roles.highest && member.roles.highest.name !== '@everyone') embed.addField('commands:userinfo.highestRole', member.roles.highest.name, true)
 
     embed.addField('commands:userinfo.createdAt', `${created.format('LL')} (${created.fromNow()})`)
 
     if (joined) embed.addField('commands:userinfo.joinedAt', `${joined.format('LL')} (${joined.fromNow()})`)
-
+    
     const msg = await send(embed)
 
     const perms = channel.permissionsFor(guild.me)
     const activity = presence && presence.activity
     const restriction = activity && (activity.type === 'LISTENING') && activity.party && activity.party.id && activity.party.id.includes('spotify:')
-    console.log(presence.activity)
+
     if (perms.has('ADD_REACTIONS') && restriction && !user.bot) {
       const spotifyEmoji = emoji('SPOTIFY', { id: true, othur: 'MUSIC' })
       const userinfoEmoji = emoji('PAGE', { id: true })
@@ -53,18 +54,17 @@ class UserInfo extends Command {
       await msg.react(userinfoEmoji)
 
       const trackName = activity.details
-      const artist = activity.state
+      const artist = activity.state.split(';').join(',')
       const album = activity.assets && activity.assets.largeText
       const image = activity.assets && activity.assets.largeImage && `https://i.scdn.co/image/${activity.assets.largeImage.replace('spotify:', '')}`
 
       const spotifyEmbed = new Embed({ author, t })
-        .attachFiles(spotifyAttachment)
-        .setTitle('commands:userinfo.spotify')
-        .setImage('attachment://' + nameAttachment)
+        .setAuthor('commands:userinfo.spotify', SPOTIFY_LOGO_PNG_URL)
         .addField('commands:userinfo.track', trackName, true)
         .addField('commands:userinfo.artist', artist, true)
         .addField('commands:userinfo.album', album, true)
         .setColor('GREEN')
+
       if (image) spotifyEmbed.setThumbnail(image)
 
       const filter = (r, u) => r.me && author.id === u.id
@@ -72,7 +72,7 @@ class UserInfo extends Command {
 
       collector.on('collect', async ({ emoji, users }) => {
         const name = emoji.id || emoji.name
-        if (perms.has('MANAGE_MESSAGES')) users.remove(user.id)
+        if (perms.has('MANAGE_MESSAGES')) await users.remove(user.id)
         if (name === spotifyEmoji) await msg.edit(spotifyEmbed)
         if (name === userinfoEmoji) await msg.edit(embed)
       })
