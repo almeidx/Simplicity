7dadcdd944ac9981c1ceec286bad435ee7ac0ce6
Almeeida <rodrigofigueire19@gmail.com>
2019-04-02 22:40:22 +0100

    fixed https://github.com/Almeeida/Simplicity/issues/62

--- src/utils/PermissionsUtils.js
+++ src/utils/PermissionsUtils.js
@@ -2,10 +2,12 @@ class PermissionsUtils {
   static verifyDev (userID, client) {
     const guildClient = client.guilds.get(process.env.SERVER_ID)
     const devRole = guildClient && guildClient.roles.get(process.env.DEV_ROLE_ID)
-    const roleCondition = devRole && !devRole.members.has(userID)
-    const idCondition = process.env.DEVS_IDS && !process.env.DEVS_IDS.includes(userID)
-    if (roleCondition || idCondition) return false
-    else return true
+
+    const roleCondition = devRole && devRole.members.has(userID)
+    const idCondition = process.env.DEVS_IDS && process.env.DEVS_IDS.includes(userID)
+
+    if (roleCondition || idCondition) return true
+    else return false
   }
 }
 
