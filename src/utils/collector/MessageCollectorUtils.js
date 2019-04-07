84ff535bb31d61abe09a0d3dc5edb8ea91bf7a0d
Tsugami <yslan3000@gmail.com>
2019-04-07 11:19:47 -0300

    add and remove running ids in MessageCollector

--- src/utils/collector/MessageCollectorUtils.js
+++ src/utils/collector/MessageCollectorUtils.js
@@ -2,41 +2,46 @@ const SimplicityEmbed = require('../../structures/Embed')
 
 class MessageCollectorUtils {
   static run (dependencies, responses, callback) {
-    const { channel, author } = dependencies
+    const { channel, author, command } = dependencies
 
-    const filter = (m) => m.author.id === author
-    const collector = channel.createMessageColletor(filter, { max: 5, time: 60000, errors: ['time'] })
+    const filter = (m) => m.author.id === author.id
+    const collector = channel.createMessageCollector(filter, { max: 5, time: 60000, errors: ['time'] })
+
+    command.running.add(channel.id, author.id)
 
     collector.on('collect', (message) => {
       if (this.checkContent(message, 'confirm')) {
         callback(dependencies)
-        this.stop('finisth')
+        collector.stop('finisth')
       } else if (this.checkContent(message, 'cancel')) {
-        this.stop('cancel')
+        collector.stop('cancel')
       } else {
-        this.incorrectResponse(dependencies, collector.collected)
+        this.incorrectResponse(dependencies, message, collector.collected)
       }
     })
 
     collector.on('end', (...params) => this.onEnd(dependencies, responses, ...params))
   }
 
-  static incorrectResponse ({ send, t }, collected) {
-    const embed = SimplicityEmbed(t)
+  static incorrectResponse ({ send, t }, message, collected) {
+    const embed = new SimplicityEmbed(t)
       .setDescription('utils:messageCollector:incorrectResponse', { count: collected.size })
-      .toText('@description')
+      .setText('@description')
 
-    send(embed).then(m => m.delete({ timeout: 10000 }))
+    send(embed).then(m => setTimeout(() => {
+      m.delete()
+      this.deleteMessage(message)
+    }, 10000))
   }
 
   static checkContent (message, query) {
-    const content = message.content.tolowerCase()
+    const content = message.content.toLowerCase()
     return (content === query) || (content.startsWith(query))
   }
 
-  static onEnd ({ t, author, send }, responses = {}, collector, reasonParam) {
-    console.log(reasonParam, this)
-    collector.forEach(message => this.deleteMessage(message))
+  static onEnd ({ t, channel, command, author, send, msg }, responses = {}, _, reasonParam) {
+    command.running.remove(channel.id, author.id)
+    msg.delete()
 
     responses = Object.assign({
       time: t('utils:messageCollector.time'),
@@ -46,7 +51,7 @@ class MessageCollectorUtils {
 
     const response = responses[reasonParam]
     if (response) {
-      const embed = new SimplicityEmbed(author)
+      const embed = new SimplicityEmbed(author, { autoAuthor: false })
         .setDescription(response)
         .setText('@description')
       send(embed).then(m => m.delete({ timeout: 60000 }))
@@ -57,7 +62,7 @@ class MessageCollectorUtils {
     const client = message.client
     const guild = message.guild
     const memberClient = guild && client && guild.member(client.user)
-    if (memberClient && message.permissionsFor(memberClient).has('MANAGE_MESSAGS')) message.delete()
+    if (memberClient && message.channel.permissionsFor(memberClient).has('MANAGE_MESSAGS')) message.delete()
   }
 }
 
