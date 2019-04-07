36b34e9881a5ae6422436d6322709f641076142f
Tsugami <yslan3000@gmail.com>
2019-04-06 23:18:04 -0300

    Update MessageCollectorUtils.js

--- src/utils/collector/MessageCollectorUtils.js
+++ src/utils/collector/MessageCollectorUtils.js
@@ -1,16 +1,47 @@
 const SimplicityEmbed = require('../../structures/Embed')
-// Only Collector Message
+
 class MessageCollectorUtils {
-  static checkContent (content, query) {
+  static run (dependencies, responses, callback) {
+    const { channel, author } = dependencies
+
+    const filter = (m) => m.author.id === author
+    const collector = channel.createMessageColletor(filter, { max: 5, time: 60000, errors: ['time'] })
+
+    collector.on('collect', (message) => {
+      if (this.checkContent(message, 'confirm')) {
+        callback(dependencies)
+        this.stop('finisth')
+      } else if (this.checkContent(message, 'cancel')) {
+        this.stop('cancel')
+      } else {
+        this.incorrectResponse(dependencies, collector.collected)
+      }
+    })
+
+    collector.on('end', (...params) => this.onEnd(dependencies, responses, ...params))
+  }
+
+  static incorrectResponse ({ send, t }, collected) {
+    const embed = SimplicityEmbed(t)
+      .setDescription('utils:messageCollector:incorrectResponse', { count: collected.size })
+      .toText('@description')
+
+    send(embed).then(m => m.delete({ timeout: 10000 }))
+  }
+
+  static checkContent (message, query) {
+    const content = message.content.tolowerCase()
     return (content === query) || (content.startsWith(query))
   }
 
   static onEnd ({ t, author, send }, responses = {}, collector, reasonParam) {
+    console.log(reasonParam, this)
     collector.forEach(message => this.deleteMessage(message))
 
     responses = Object.assign({
-      time: t('utils:commandTimedout'),
-      cancel: t('utils:cancelled')
+      time: t('utils:messageCollector.time'),
+      cancel: t('utils:messageCollector.cancel'),
+      limit: t('utils:messageCollector.limit')
     }, responses)
 
     const response = responses[reasonParam]
@@ -18,7 +49,7 @@ class MessageCollectorUtils {
       const embed = new SimplicityEmbed(author)
         .setDescription(response)
         .setText('@description')
-      return send(embed)
+      send(embed).then(m => m.delete({ timeout: 60000 }))
     }
   }
 
