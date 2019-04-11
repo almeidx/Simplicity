c7d141912743e0d4b903e3b8c0b37e788cb7621f
Tsugami <yslan3000@gmail.com>
2019-04-11 13:27:46 -0300

    using new subcommands

--- src/commands/bot/config.js
+++ src/commands/bot/config.js
@@ -1,4 +1,6 @@
-const { Command, CommandContext, SimplicityEmbed } = require('../..')
+const { Command, SimplicityEmbed } = require('../..')
+const PrefixCommand = require('./prefix')
+const LanguageCommand = require('./language')
 
 class Config extends Command {
   constructor (client) {
@@ -6,45 +8,14 @@ class Config extends Command {
     this.category = 'bot'
     this.aliases = ['configuration', 'serversettings', 's', 'serverconfig', 'serverconfiguration']
     this.requirements = { permissions: ['MANAGE_GUILD'] }
+    this.subcommands = [new PrefixCommand(client, { aliases: ['p'] }), new LanguageCommand(client, { aliases: ['l'] })]
   }
 
-  async run ({ args, author, client, guild, message, prefix, send, t }) {
+  run ({ author, prefix, send, t, language }) {
     const embed = new SimplicityEmbed({ author, t })
-
-    let data = await client.database.guilds.get(guild.id)
-    if (!data) data = await client.database.guilds.create(guild.id)
-
-    const key = args && args.length && args.shift().toLowerCase()
-    const value = args && args.join(' ')
-
-    const langArray = []
-    for (let x = 0; x < 3; x++) {
-      langArray.push(t(`commands:config.languageKeys.${x}`))
-    }
-    const prefixArray = []
-    for (let x = 0; x < 2; x++) {
-      prefixArray.push(t(`commands:config.prefixKeys.${x}`))
-    }
-
-    if (!key) {
-      embed
-        .addField('» $$commands:config.prefix', data.prefix || process.env.PREFIX)
-        .addField('» $$commands:config.language', data.lang || process.env.DEFAULT_LANG)
-      return send(embed)
-    } else if (langArray.includes(key)) {
-      const LanguageCommand = client.commands.fetch('language')
-
-      LanguageCommand._run(new CommandContext({ args, author, client, guild, message, query: value, prefix, send, t }))
-    } else if (prefixArray.includes(key)) {
-      const PrefixCommand = client.commands.fetch('prefix')
-
-      PrefixCommand._run(new CommandContext({ args, author, client, guild, message, query: value, prefix, send, t }))
-    } else {
-      embed
-        .addField('» $$commands:config.prefix', data.prefix || process.env.PREFIX)
-        .addField('» $$commands:config.language', data.lang || process.env.DEFAULT_LANG)
-      return send(embed)
-    }
+      .addField('» $$commands:config.prefix', prefix)
+      .addField('» $$commands:config.language', language)
+    return send(embed)
   }
 }
 
