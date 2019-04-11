95d2eebad40aeb3c30ed8b4e5865a70de602548d
Tsugami <yslan3000@gmail.com>
2019-04-11 13:36:23 -0300

    Added more aliases

--- src/commands/bot/config.js
+++ src/commands/bot/config.js
@@ -2,13 +2,16 @@ const { Command, SimplicityEmbed } = require('../..')
 const PrefixCommand = require('./prefix')
 const LanguageCommand = require('./language')
 
+const aliasesPrefix = ['p', 'setp', 'setprefix']
+const aliasesLanguage = ['l', 'lang', 'setlang', 'setlanguage', 'setl']
+
 class Config extends Command {
   constructor (client) {
     super(client)
     this.category = 'bot'
     this.aliases = ['configuration', 'serversettings', 's', 'serverconfig', 'serverconfiguration']
     this.requirements = { permissions: ['MANAGE_GUILD'] }
-    this.subcommands = [new PrefixCommand(client, { aliases: ['p'] }), new LanguageCommand(client, { aliases: ['l'] })]
+    this.subcommands = [new PrefixCommand(client, aliasesPrefix), new LanguageCommand(client, aliasesLanguage)]
   }
 
   run ({ author, prefix, send, t, language }) {
