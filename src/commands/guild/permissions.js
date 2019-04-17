a33a937519ddaee329f49f61f035bed85de722bb
Almeeida <rodrigofigueire19@gmail.com>
2019-04-17 15:57:22 +0100

    clean

--- src/commands/guild/permissions.js
+++ src/commands/guild/permissions.js
@@ -1,5 +1,5 @@
 const { Command, CommandError, Constants, Parameters, SimplicityEmbed } = require('../..')
-const { ADMINISTRATOR_PERMISSION, NORMAL_PERMISSIONS, PERMISSIONS } = Constants
+const { MANAGER_PERMISSIONS } = Constants
 const { UserParameter } = Parameters
 const checkTick = (c) => c ? 'TICK_YES' : 'TICK_NO'
 const optionsParameter = {
@@ -24,22 +24,11 @@ class Permissions extends Command {
 		const member = user && guild.member(user)
 		if (!member) throw new CommandError('errors:invalidUser')
 
-		const memberPermissions = member && member.permissions && member.permissions.toArray().filter(p => !NORMAL_PERMISSIONS.includes(p))
-		const resultAdministrator = memberPermissions && memberPermissions.includes(ADMINISTRATOR_PERMISSION) && t('permissions:' + ADMINISTRATOR_PERMISSION)
-		const resultAllPermissions = memberPermissions && memberPermissions.sort((a, b) => PERMISSIONS.indexOf(a) - PERMISSIONS.indexOf(b))
-		const resultPermissions = memberPermissions && (resultAdministrator || (resultAllPermissions && resultAllPermissions))
-
 		const embed = new SimplicityEmbed({ author, emoji, t })
 			.setAuthor('commands:permissions.author', user.displayAvatarURL(), null, { user: user.tag })
 
-		if (resultPermissions) {
-			if (Array.isArray(resultPermissions)) {
-				resultPermissions.forEach(p => {
-					embed.addField(`permissions:${p}`, `#${checkTick(member.permissions.has(p))}`, true)
-				})
-			} else {
-        embed.addField(`permissions:${resultPermissions.toUpperCase()}`, `#${checkTick(member.permissions.has(resultPermissions.toUpperCase()))}`)
-			}
+		for (const p of MANAGER_PERMISSIONS) {
+			embed.addField(`permissions:${p}`, `#${checkTick(member.permissions.has(p))}`, true)
 		}
 
 		return send(embed)
