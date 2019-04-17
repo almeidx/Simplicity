7d39cb3db8b65c370d5e864b1c7ede23aeab7ea6
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-04-17 11:36:00 +0100

    array spacing kek

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { SimplicityClient } = require('./src')
 const client = new SimplicityClient({
   fetchAllMembers: true,
   disableEveryone: true,
-  disabledEvents: ['TYPING_START'],
-  partials: ['MESSAGE', 'CHANNEL']
+  disabledEvents: [ 'TYPING_START' ],
+  partials: [ 'MESSAGE', 'CHANNEL' ]
 })
 
 client.login(process.env.BOT_TOKEN).catch((e) => console.error(e))
