c4241686cfbc22a3f906973fc7177cafa263a0e1
Almeeida <rodrigofigueire19@gmail.com>
2019-04-20 01:46:27 +0100

    init and finish

---
+++ src/commands/utility/discriminator.js
@@ -0,0 +1,33 @@
+const { Command, CommandError, SimplicityEmbed, Utils } = require('../../')
+const { getServerIconURL } = Utils
+
+class Discriminator extends Command {
+  constructor (client) {
+    super(client)
+    this.aliases = ['discrim']
+    this.category = 'util'
+    this.requirements = {
+      guildOnly: true
+    }
+  }
+
+  async run ({ author, client, emoji, guild, query, send, t }) {
+    let discrim
+    if (!query) discrim = author.discriminator
+    else discrim = query.replace(/#/g, '')
+
+    let users = guild.members.filter((m) => m.user.discriminator === discrim)
+    if (!users || !users.size) throw new CommandError('commands:discriminator.nobody')
+
+    const mapped = users.map((u) => u.user.tag)
+    if (!mapped) throw new CommandError('commands:discriminator.nobody')
+
+    const final = mapped.slice(0, 25).join('\n') + (mapped.size > 25 ? '...' : '')
+    const embed = new SimplicityEmbed({ author, t }, { autoAuthor: false })
+      .setAuthor('commands:discriminator.users', getServerIconURL(guild), null, { discrim })
+      .setDescription(final)
+    return send(embed)
+  }
+}
+
+module.exports = Discriminator
