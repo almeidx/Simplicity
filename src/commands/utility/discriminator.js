dee608eedcac083e68d77270accfc27276884ed1
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-04-20 01:55:51 +0100

    more simplification

--- src/commands/utility/discriminator.js
+++ src/commands/utility/discriminator.js
@@ -18,9 +18,9 @@ class Discriminator extends Command {
 
     const users = guild.members.filter((m) => m.user.discriminator === discrim)
     const mapped = users && users.map((u) => u.user.tag)
-    if (!users || !users.size || !mapped) throw new CommandError('commands:discriminator.nobody')
+    const final = mapped && mapped.slice(0, 25).join('\n') + (mapped.size > 25 ? '...' : '')
+    if (!users || !users.size || !mapped || !final) throw new CommandError('commands:discriminator.nobody')
 
-    const final = mapped.slice(0, 25).join('\n') + (mapped.size > 25 ? '...' : '')
     const embed = new SimplicityEmbed({ author, t }, { autoAuthor: false })
       .setAuthor('commands:discriminator.users', getServerIconURL(guild), null, { discrim })
       .setDescription(final)
