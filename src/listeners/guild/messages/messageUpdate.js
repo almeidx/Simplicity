044bc572fc02a474851b121311f1ac8ba00ec24b
Almeeida <rodrigofigueire19@gmail.com>
2019-04-25 00:19:32 +0100

    Converting MessageUpdate

--- src/listeners/guild/messages/messageUpdate.js
+++ src/listeners/guild/messages/messageUpdate.js
@@ -1,46 +1,47 @@
 const { SimplicityEmbed, SimplicityListener, Constants } = require('../../../index')
-const clean = (str) => str.slice(0, 1020) + str.length >= 1024 ? '...' : str
+const clean = (str) => str.slice(0, 1020) + (str.length >= 1024 ? '...' : str)
 
 class MessageUpdate extends SimplicityListener {
   constructor (client) {
     super(client)
   }
 
-  async on (client, oldMessage, newMessage) {
+  async on (_, oldMessage, newMessage) {
     if (oldMessage.guild.me.permissions.has('READ_AUDIT_LOGS')) {
-      const { t } = await client.database.guilds.get(oldMessage.guild.id)
       const url = oldMessage.url
       const user = oldMessage.author
       const msgChannel = oldMessage.channel
       const oldContent = oldMessage.content
       const newContent = newMessage.content
 
-      const embed = new SimplicityEmbed({ author: user, t }, { autoFooter: false })
+      const embed = new SimplicityEmbed(this.getFixedT(oldMessage.guild.id))
         .setTimestamp()
-        .setFooter(`ID: ${user.id}`)
+        .setFooter('loggers:id', '', { id: user.id })
 
       // MESSAGE EDITED
       if (oldContent !== newContent) {
-        embed.setDescription('loggers:messageEdited', { url, user, channel: msgChannel })
-          .addField('loggers:before', clean(oldContent) || 'errors:general', true)
-          .addField('loggers:after', clean(newContent) || 'errors:general', true)
+        embed
+          .setDescription('loggers:messageEdited', { url, user, channel: msgChannel })
+          .addField('loggers:before', clean(oldContent) || 'loggers:messageError', true)
+          .addField('loggers:after', clean(newContent) || 'loggers:messageError', true)
           .setColor(Constants.COLORS.MESSAGE_EDIT)
-        return this.sendMessage('messageEdit', embed).catch(() => null)
+        return this.sendLogMessage(oldMessage.guild.id, 'MessageEdit', embed).catch(() => null)
       } else
-
       // MESSAGE PINS
       if (oldMessage.pinned !== newMessage.pinned) {
-        // MESSAGE UNPINNED
-        if (oldMessage.pinned && !newMessage.pinned) {
-          embed.setDescription('loggers:messageUnpinned', { url, user, channel: msgChannel })
-            .setColor(Constants.COLORS.MESSAGE_UNPIN)
-          return this.sendMessage('messageUnpin', embed).catch(() => null)
-        } else
         // MESSAGE PINNED
         if (!oldMessage.pinned && newMessage.pinned) {
-          embed.setDescription('loggers:messagePinned', { url, user, channel: msgChannel })
+          embed
+            .setDescription('loggers:messagePinned', { url, user, channel: msgChannel })
             .setColor(Constants.COLORS.MESSAGE_PIN)
-          return this.sendMessage('messagePin', embed).catch(() => null)
+          return this.sendLogMessage(oldMessage.guild.id, 'MessagePin', embed).catch(() => null)
+        } else
+        // MESSAGE UNPINNED
+        if (oldMessage.pinned && !newMessage.pinned) {
+          embed
+            .setDescription('loggers:messageUnpinned', { url, user, channel: msgChannel })
+            .setColor(Constants.COLORS.MESSAGE_UNPIN)
+          return this.sendLogMessage(oldMessage.guild.id, 'MessageUnpin', embed).catch(() => null)
         }
       }
     }
