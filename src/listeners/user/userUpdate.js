d9c9b0fa70cb5e9031fc5fa5b9ae86f869954223
Almeeida <rodrigofigueire19@gmail.com>
2019-04-25 00:29:43 +0100

    Avatar Updates convert

--- src/listeners/user/userUpdate.js
+++ src/listeners/user/userUpdate.js
@@ -6,19 +6,17 @@ class UserUpdate extends SimplicityListener {
   }
 
   async on (client, oldUser, newUser) {
-    const { t } = await client.database.guilds.get(oldUser.guild.id)
   // AVATAR CHANGES
     if (oldUser.displayAvatarURL() !== newUser.displayAvatarURL()) {
-      client.guilds.filter(guild => guild.members.get(oldUser.id)).forEach(guild => {
-        this.sendMessage('channel_log_start',
-          new SimplicityEmbed({ t })
+      client.guilds.filter((g) => g.members.get(oldUser.id)).forEach((guild) => {
+        this.sendLogMessage(guild.id, 'UserAvatarChange',
+          new SimplicityEmbed(this.getFixedT(guild.id))
             .setTimestamp()
             .setAuthor(newUser.tag, newUser.displayAvatarURL())
             .setImage(newUser.displayAvatarURL({ size: 2048 }))
             .setColor(process.env.COLOR)
             .setFooter(guild.name, guild.iconURL())
-            .setDescription('loggers:changedAvatar', { user: newUser })
-        )
+            .setDescription('loggers:changedAvatar', { user: newUser })).catch(() => null)
       })
     }
   }
