52e6d2023fc02a007d97abc39e2314e5a3df7c91
Almeeida <rodrigofigueire19@gmail.com>
2019-04-25 00:44:32 +0100

    só spawnar essa gambiarra aqui

--- src/listeners/user/userUpdate.js
+++ src/listeners/user/userUpdate.js
@@ -6,7 +6,7 @@ class UserUpdate extends SimplicityListener {
   }
 
   async on (client, oldUser, newUser) {
-  // AVATAR CHANGES
+    // AVATAR CHANGES
     if (oldUser.displayAvatarURL() !== newUser.displayAvatarURL()) {
       client.guilds.filter((g) => g.members.get(oldUser.id)).forEach((guild) => {
         this.sendLogMessage(guild.id, 'UserAvatarChange',
@@ -17,6 +17,32 @@ class UserUpdate extends SimplicityListener {
             .setColor(process.env.COLOR)
             .setFooter(guild.name, Utils.getServerIconURL(guild))
             .setDescription('loggers:changedAvatar', { user: newUser })).catch(() => null)
+      })
+    } else if (oldUser.username !== newUser.username) {
+      client.guilds.filter((g) => g.members.get(oldUser.id)).forEach((guild) => {
+        this.sendLogMessage(guild.id, 'UserUsernameChange',
+          new SimplicityEmbed(this.getFixedT(guild.id))
+            .setTimestamp()
+            .setAuthor(newUser.tag, newUser.displayAvatarURL())
+            .setColor(process.env.COLOR)
+            .setThumbnail(oldUser.displayAvatarURL())
+            .setFooter(guild.name, Utils.getServerIconURL(guild))
+            .setDescription('loggers:changedUsername', { user: newUser })).catch(() => null)
+            .addField('loggers:before', oldUser.username, true)
+            .addField('loggers:after', newUser.username, true)
+      })
+    } else if (oldUser.discriminator !== newUser.discriminator) {
+      client.guilds.filter((g) => g.members.get(oldUser.id)).forEach((guild) => {
+        this.sendLogMessage(guild.id, 'UserDiscriminatorChange',
+          new SimplicityEmbed(this.getFixedT(guild.id))
+            .setTimestamp()
+            .setAuthor(newUser.tag, newUser.displayAvatarURL())
+            .setColor(process.env.COLOR)
+            .setThumbnail(oldUser.displayAvatarURL())
+            .setFooter(guild.name, Utils.getServerIconURL(guild))
+            .setDescription('loggers:changedDiscriminator', { user: newUser })).catch(() => null)
+            .addField('loggers:before', oldUser.discriminator, true)
+            .addField('loggers:after', newUser.discriminator, true)
       })
     }
   }
