1b107afcaaa1c857d97141690b37276f6ab38032
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-04-26 23:06:20 +0100

    removed shit that was breaking the bot for now

--- src/commands/guild/addemoji.js
+++ src/commands/guild/addemoji.js
@@ -1,13 +1,11 @@
-const { SimplicityEmbed, Command, MessageUtils, CommandError, MessageCollectorUtils, Parameters } = require('../..')
+const { SimplicityEmbed, Command, MessageUtils, CommandError, Parameters } = require('../..')
 const { StringParameter } = Parameters
 
-const INVALID_CHARACTERS_REGEX = /[^a-z0-9_]/gi
-
-const options = {
+const StringParameterOptions = {
   defaultString: 'emoji',
   maxLength: 32,
   minLength: 2,
-  regex: INVALID_CHARACTERS_REGEX,
+  regex: /[^a-z0-9_]/gi,
   errors: {
     maxLength: 'commands:addemoji:nameTooBig',
     minLength: 'commands:addemoji:nameTooShort',
@@ -28,31 +26,17 @@ class AddEmoji extends Command {
   }
 
   async run ({ args, author, channel, message, send, totalLength, t, guild }) {
-    const name = await StringParameter.parse(args[0], options)
-    const image = (await MessageUtils.getImage(message, totalLength)) || (await MessageUtils.fetchImage(channel))
-    if (!image) throw new CommandError('commands:addemoji:noNameLink', { onUsage: true })
+    const name = await StringParameter.parse(args[0], StringParameterOptions)
+    const image = await MessageUtils.getImage(message, totalLength) || await MessageUtils.fetchImage(channel)
+    if (!image || !name) throw new CommandError('commands:addemoji:noNameLink', { onUsage: true })
 
-    const embed = new SimplicityEmbed({ author, t }, { autoAuthor: false })
-      .setDescription('commands:addemoji.waitingResponse')
-      .setThumbnail(image)
-    const msg = await send(embed)
+    const emoji = await guild.emojis.create(image, name).catch(() => null)
+    if (!emoji) throw new CommandError('commands:addemoji.error')
 
-    const dependencies = { msg, command: this, channel, author, send, t }
-    const responses = { cancel: t('commands:addemoji:cancelled') }
-
-    await MessageCollectorUtils.run(dependencies, responses, async () => {
-      const emoji = await guild.emojis.create(image, name).catch(error => { return send(t(`errors:${error.codo}`)) })
-
-      const embedTitle = emoji ? 'commands:addemoji.success' : 'errors:oops'
-      const embedDescription = 'commands:addemoji.' + (emoji ? 'emojiCreated' : 'error')
-      const embedColor = emoji ? process.env.COLOR : 'RED'
-
-      const embed = new SimplicityEmbed({ author, t }, { autoAuthor: !!emoji })
-        .setDescription(embedDescription, { emoji: emoji.toString() })
-        .setTitle(embedTitle)
-        .setColor(embedColor)
-      return send(embed)
-    })
+    const embed = new SimplicityEmbed({ author, t })
+      .setDescription('commands:addemoji.emojiCreated', { emoji: emoji.toString() })
+      .setTitle('commands:addemoji.success')
+    return send(embed)
   }
 }
 
