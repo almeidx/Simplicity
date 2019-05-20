414d21acc3292eb9718a74a9891e36b83267592a
Almeeida <rodrigofigueire19@gmail.com>
2019-05-20 23:19:56 +0100

    cleaning userinfo oof

--- src/commands/utility/userinfo.js
+++ src/commands/utility/userinfo.js
@@ -1,30 +1,29 @@
 const { Command, SimplicityEmbed, Constants, Parameters, PermissionsUtils, Utils } = require('../../')
 const { SPOTIFY_LOGO_PNG_URL, PERMISSIONS, ADMINISTRATOR_PERMISSION, NORMAL_PERMISSIONS } = Constants
+const { getJoinPosition } = Utils
 const { UserParameter } = Parameters
 const moment = require('moment')
 
 class UserInfo extends Command {
   constructor (client) {
     super(client)
-    this.aliases = [ 'ui', 'user' ]
+    this.aliases = [ 'ui', 'user', 'userinformation', 'infouser', 'informationuser' ]
     this.category = 'util'
     this.requirements = { clientPermissions: [ 'EMBED_LINKS' ] }
   }
 
   async run ({ author, client, channel, emoji, guild, query, send, t }) {
-    const user = (!query ? author : await UserParameter.parse(query, {
+    const user = !query ? author : await UserParameter.parse(query, {
       errors: { missingError: 'errors:invalidUser' },
       required: true
-    }, { client, guild }))
+    }, { client, guild })
 
-    const titles = []
+    const titles = [user.tag]
+    if (guild && guild.ownerID === user.id) titles.push('#crown')
+    if (PermissionsUtils.verifyDev(user.id, client)) titles.push('#developer')
+    if (user.bot) titles.push('#bot')
 
-    titles.push(user.tag)
-    if (guild && guild.ownerID === user.id) titles.push(`#crown`)
-    if (PermissionsUtils.verifyDev(user.id, client)) titles.push(`#developer`)
-    if (user.bot) titles.push(`#bot`)
-
-    const joinPosition = Utils.getJoinPosition(user.id, guild)
+    const joinPosition = getJoinPosition(user.id, guild)
     const member = guild && guild.member(user)
     const nickname = member && member.nickname
     const created = moment(user.createdAt)
@@ -34,45 +33,54 @@ class UserInfo extends Command {
     const clientStatus = presence && presence.clientStatus
     const status = clientStatus && (clientStatus.desktop || clientStatus.mobile || clientStatus.web)
 
-    const highestRole = member && member.roles && member.roles.highest && (member.roles.highest.id !== guild.id) && member.roles.highest
+    const highestRole = member && member.roles.highest.id !== guild.id && member.roles.highest
     const roles = member && member.roles && member.roles.sort((a, b) => b.position - a.position).map(r => r).slice(0, -1)
     const rolesClean = roles && roles.map(r => r.name || r.toString())
     const activity = presence && presence.activity
     const activityType = activity && activity.type && activity.name
 
-    const embed = new SimplicityEmbed({ author, t, emoji, autoAuthor: false })
+    const embed = new SimplicityEmbed({ author, t, emoji }, { autoAuthor: false })
       .setAuthor(titles.join(' '), user.displayAvatarURL())
       .setThumbnail(user)
       .addField('» $$commands:userinfo.username', user.tag, true)
 
-    if (nickname) embed.addField('» $$commands:userinfo.nickname', nickname, true)
+    if (nickname)
+      embed.addField('» $$commands:userinfo.nickname', nickname, true)
 
     embed.addField('» $$commands:userinfo.id', user.id, true)
 
-    if (status) embed.addField('» $$commands:userinfo.status', `#${presence.status} $$common:status.${presence.status}`, true)
-    if (highestRole && roles.length > 5) embed.addField('» $$commands:userinfo.highestRole', highestRole.name || highestRole.toString(), true)
-    if (activityType) embed.addField('» $$common:activityType.' + activity.type, activity.name, true)
-    if (rolesClean && rolesClean.length <= 5 && rolesClean.join(', ')) embed.addField('» $$commands:userinfo.roles', rolesClean.join(', '), true)
-    if (joinPosition) embed.addField('» $$commands:userinfo.joinPosition', joinPosition, true)
+    if (status)
+      embed.addField('» $$commands:userinfo.status', `#${presence.status} $$common:status.${presence.status}`, true)
+    if (highestRole && roles.length > 5)
+      embed.addField('» $$commands:userinfo.highestRole', highestRole.name || highestRole.toString(), true)
+    if (activityType)
+      embed.addField(`» $$common:activityType.${activity.type}`, activity.name, true)
+    if (rolesClean && rolesClean.length <= 5)
+      embed.addField('» $$commands:userinfo.roles', rolesClean.join(', '), true)
+    if (joinPosition)
+      embed.addField('» $$commands:userinfo.joinPosition', joinPosition, true)
 
     embed.addField('» $$commands:userinfo.createdAt', `${created.format('LL')} (${created.fromNow()})`)
 
     if (joined) embed.addField('» $$commands:userinfo.joinedAt', `${joined.format('LL')} (${joined.fromNow()})`)
 
-    // PERMISSIONS
     const memberPermissions = member && member.permissions && member.permissions.toArray().filter(p => !NORMAL_PERMISSIONS.includes(p))
-    const resultAdministrator = memberPermissions && memberPermissions.includes(ADMINISTRATOR_PERMISSION) && t('permissions:' + ADMINISTRATOR_PERMISSION)
-    const resultAllPermissions = memberPermissions && memberPermissions.sort((a, b) => PERMISSIONS.indexOf(a) - PERMISSIONS.indexOf(b))
-    const resultPermissions = memberPermissions && (resultAdministrator || (resultAllPermissions && resultAllPermissions.map(p => t('permissions:' + p)).join(', ')))
+    let resultAdministrator, resultAllPermissions, resultPermissions
 
-    if (resultPermissions) embed.addField('» $$commands:userinfo.permissions', resultPermissions)
+    if (memberPermissions) {
+      resultAdministrator = memberPermissions.includes(ADMINISTRATOR_PERMISSION) && t('permissions:' + ADMINISTRATOR_PERMISSION)
+      resultAllPermissions = memberPermissions.sort((a, b) => PERMISSIONS.indexOf(a) - PERMISSIONS.indexOf(b))
+      resultPermissions = resultAdministrator || (resultAllPermissions && resultAllPermissions.map(p => t('permissions:' + p)).join(', '))
+    }
+
+    if (resultPermissions)
+      embed.addField('» $$commands:userinfo.permissions', resultPermissions)
 
     const message = await send(embed)
-
     const checkPermissions = (p) => channel.permissionsFor(guild.me).has(p)
-    const spotifyRestriction = activity && (activity.type === 'LISTENING') && activity.party && activity.party.id && activity.party.id.includes('spotify:')
 
     if (checkPermissions('ADD_REACTIONS') && checkPermissions('READ_MESSAGE_HISTORY')) {
+      const spotifyRestriction = activity && activity.party && activity.party.id && activity.party.id.includes('spotify:')
       let spotify, role
 
       if (spotifyRestriction && !user.bot) {
@@ -102,14 +110,19 @@ class UserInfo extends Command {
           const name = emoji.id || emoji.name
           const checkEmbed = (e) => e.author.name === message.embeds[0].author.name
 
-          if (checkPermissions('MANAGE_MESSAGES')) await users.remove(user.id)
-          if (spotify && name === spotify.emoji && !checkEmbed(spotify.embed)) await message.edit(spotify.embed)
-          if (name === userinfoEmoji && !checkEmbed(embed)) await message.edit(embed)
-          if (role && name === role.emoji && !checkEmbed(role.embed)) await message.edit(role.embed)
+          if (checkPermissions('MANAGE_MESSAGES'))
+            await users.remove(user)
+          if (spotify && spotify.emoji === name && !checkEmbed(spotify.embed))
+            await message.edit(spotify.embed)
+          if (name === userinfoEmoji && !checkEmbed(embed))
+            await message.edit(embed)
+          if (role && role.emoji === name && !checkEmbed(role.embed))
+            await message.edit(role.embed)
         })
 
         collector.on('end', async () => {
-          if (message && checkPermissions('MANAGE_MESSAGES')) await message.reactions.removeAll().catch(() => {})
+          if (message && checkPermissions('MANAGE_MESSAGES'))
+            await message.reactions.removeAll().catch(() => null)
         })
       }
     }
@@ -120,7 +133,8 @@ function createEmbedSpotify (activity, embedOptions) {
   const trackName = activity.details
   const artist = activity.state.split(';').join(',')
   const album = activity.assets && activity.assets.largeText
-  const image = activity.assets && activity.assets.largeImage && `https://i.scdn.co/image/${activity.assets.largeImage.replace('spotify:', '')}`
+  const largeImage = activity.assets && activity.assets.largeImage
+  const image = largeImage && `https://i.scdn.co/image/${largeImage.replace('spotify:', '')}`
 
   const embed = new SimplicityEmbed(embedOptions)
     .setAuthor('commands:userinfo.spotify', SPOTIFY_LOGO_PNG_URL)
