c9fa259d3fb0efa2204f335ace860de79cb2fd8b
Almeeida <rodrigofigueire19@gmail.com>
2019-06-11 00:34:25 +0100

    security

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ const client = new SimplicityClient({
 })
 
 client.login(process.env.BOT_TOKEN).catch(console.error)
+
+process.on('uncaughtException', (err) => {
+  const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './')
+  console.error('Uncaught Exception: ', errorMsg)
+  process.exit(1)
+})
+
+process.on('unhandledRejection', (err) => {
+  console.error('Uncaught Promise Error: ', err)
+})
