12fcb8086566c0525fdc79878dad34df56f76e66
Almeeida <42935195+Almeeida@users.noreply.github.com>
2019-06-12 19:39:02 +0100

    there now are animated icons

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -11,7 +11,7 @@ class Utils {
   }
 
   static getServerIconURL (guild) {
-    if (guild.iconURL()) return guild.iconURL({ format: 'png', size: 2048 })
+    if (guild.iconURL()) return guild.iconURL({ size: 2048 })
     else return `https://guild-default-icon.herokuapp.com/${guild.nameAcronym}`
   }
 
