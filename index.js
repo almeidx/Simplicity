64e176ead7d6c19c9893c4ce9c933cb9f0979341
Almeida <almeida@dbm-network.org>
2019-08-21 12:19:25 +0100

    debug event and some more stuff

--- index.js
+++ index.js
@@ -18,40 +18,32 @@ const CLIENT_OPTIONS = {
   },
 };
 
-const express = require('express');
-const keepalive = require('express-glitch-keepalive');
-const app = express();
-
-app.use(keepalive);
-
-app.get('/', (req, res) => {
-  res.json('How did you get here?');
-});
-
-app.get('/', (request, response) => {
-  response.sendStatus(200);
-});
-
-app.listen(process.env.PORT);
-
 const client = new SimplicityClient(CLIENT_OPTIONS);
-client.login().catch(console.error);
+client.login().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
 
 client
-  .on('disconnect', () => console.log('Bot is disconnecting...'))
-  .on('reconnect', () => console.log('Bot reconnecting...'))
-  .on('warn', (warn) => console.log(warn))
+  .on('shardReady', (id) => console.log(`Shard ${id} is ready`))
+  .on('shardDisconnected', (event, id) => console.error(`Shard ${id} has been disconnected.`, event))
+  .on('shardReconnecting', (id) => console.log(`Shard ${id} has reconnected.`))
+  .on('shardResumed', (id, replayedEvents) => console.log(`Shard ${id} has resumed. Replayed events:`, replayedEvents))
+  .on('shardError', (error, shardID) => console.error(`Shard ${shardID} Error:`, error))
+  .on('warn', (info) => console.log(info))
+  .on('debug', (info) => console.log(info))
   .on('invalidated', () => {
     console.log('The client\'s session is now invalidated.');
     process.exit(1);
   });
 
 process
+  .on('unhandledRejection', (error) => console.error('Uncaught Promise Error:', error))
+  .on('beforeExit', (code) => console.log('Process beforeExit event with code: ', code))
+  .on('exit', (code) => console.log('Process exit event with code: ', code))
+  .on('disconnect', () => console.log('Process is disconnecting'))
   .on('uncaughtException', (error) => {
     const msg = error.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
     console.error('Uncaught Exception:', msg);
     process.exit(1);
-  })
-  .on('unhandledRejection', (error) => {
-    console.error('Uncaught Promise Error:', error);
   });
