cb721a399913793c59474d5ba578af5132e4763d
Almeida <rodrigofigueire19@gmail.com>
2019-08-30 19:16:44 +0100

    sla mais

--- index.js
+++ index.js
@@ -25,13 +25,7 @@ client.login().catch((error) => {
 });
 
 client
-  .on('shardReady', (id) => console.log(`Shard ${id} is ready`))
-  .on('shardDisconnected', (event, id) => console.error(`Shard ${id} has been disconnected.`, event))
-  .on('shardReconnecting', (id) => console.log(`Shard ${id} has reconnected.`))
-  .on('shardResumed', (id, replayedEvents) => console.log(`Shard ${id} has resumed. Replayed events:`, replayedEvents))
   .on('shardError', (error, shardID) => console.error(`Shard ${shardID} Error:`, error))
-  .on('warn', (info) => console.log(info))
-  .on('debug', (info) => console.log(info))
   .on('invalidated', () => {
     console.log('The client\'s session is now invalidated.');
     process.exit(1);
@@ -39,9 +33,6 @@ client
 
 process
   .on('unhandledRejection', (error) => console.error('Uncaught Promise Error:', error))
-  .on('beforeExit', (code) => console.log('Process beforeExit event with code: ', code))
-  .on('exit', (code) => console.log('Process exit event with code: ', code))
-  .on('disconnect', () => console.log('Process is disconnecting'))
   .on('uncaughtException', (error) => {
     const msg = error.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
     console.error('Uncaught Exception:', msg);
