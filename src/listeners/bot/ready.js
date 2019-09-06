e1960c59f041ad5a7f5ac91f44d3dae704f36a1e
Almeida <rodrigofigueire19@gmail.com>
2019-09-06 13:09:28 +0100

    como assim era isso

--- src/listeners/bot/ready.js
+++ src/listeners/bot/ready.js
@@ -10,7 +10,6 @@ class ReadyListener extends SimplicityListener {
   on(client) {
     const message = `Logged on ${client.guilds.size} guilds and ${client.users.size} users`;
     client.logger.success('Ready', message);
-    setInterval(() => client.user.setActivity(`@${client.user.username} help`, 21600000));
 
     this.sendPrivateMessage('bot_log',
       new SimplicityEmbed({ author: client.user })
