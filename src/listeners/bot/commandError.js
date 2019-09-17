994f510570f1d6190ec6bc4e1a11c61f0455f44a
Almeeida <rodrigofigueire19@gmail.com>
2019-09-17 19:38:38 +0100

    Typos

--- src/listeners/bot/commandError.js
+++ src/listeners/bot/commandError.js
@@ -13,7 +13,7 @@ class CommandErrorListener extends SimplicityListener {
     if (!(error instanceof CommandError)) {
       console.error(error);
       const errorTranslation = error.code && getTranslation(`errors:${error.code}`);
-      const errorMessage = errorTranslation || t('errors:errorCommand');
+      const errorMessage = errorTranslation || t('api_errors:errorCommand');
       this.sendErrorCommandMessage(errorMessage, false, { send, canEmbed, author, t, command });
 
       const embed = new SimplicityEmbed(author, { type: 'error' })
