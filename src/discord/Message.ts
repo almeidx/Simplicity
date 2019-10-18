8364094c1fff503c8acbe00cf0dc166c06eb9739
Tsugami <yslan3000@gmail.com>
2019-10-18 16:02:11 -0300

    add: Custom Message

---
+++ src/discord/Message.ts
@@ -0,0 +1,30 @@
+import {
+  Structures, StringResolvable, APIMessage, MessageOptions, MessageEditOptions,
+} from 'discord.js';
+import SimplicityClient from 'src/bot/client/SimplicityClient';
+
+type contentType = StringResolvable | APIMessage
+type optionsType = MessageEditOptions | MessageOptions
+
+export default Structures.extend('Message', (DiscordMessage) => {
+  class SimplicityMessage extends DiscordMessage {
+    public readonly client: SimplicityClient
+
+    async send(content: contentType, options: optionsType) {
+      const msgId = this.client.commandMessages.get(this.id);
+      const message = msgId && await this.channel.messages.fetch(msgId);
+      if (message) {
+        return message.edit(content, options);
+      }
+      return this.channel.send(content, options);
+    }
+  }
+
+  return SimplicityMessage;
+});
+
+declare module 'discord.js' {
+  interface Message {
+  send(content: contentType, options?: optionsType): Promise<Message | any>;
+  }
+}
