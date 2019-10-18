422e5f485af09c4e4c3d984ad3413a0b28ed2262
Tsugami <yslan3000@gmail.com>
2019-10-18 14:14:21 -0300

    add(ready.ts): statistics and set presence

--- src/bot/listeners/ready.ts
+++ src/bot/listeners/ready.ts
@@ -1,6 +1,14 @@
+import { PresenceData } from 'discord.js';
 import SimplicityClient from '../client/SimplicityClient';
 
 export const name = 'ready';
 export function handle<SimplicityClient>(): void {
-  this.logger.log(`My name is ${this.user.username}!`);
+  this.logger.log(
+    'Estatísticas:',
+    `Logado com ${this.guilds.size} servidores e ${this.users.size} usuários`,
+  );
+
+  this.user.setPresence({ activity: { name: `@${this.user.username} help` }, status: 'idle' })
+    .then((presence: PresenceData) => this.logger.log('Presence foi definido como:', presence.activity.name))
+    .catch((error: Error) => this.logger.error('Não foi possivel setar o Presence', error));
 }
