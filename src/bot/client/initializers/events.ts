27f9c598c391bba2feba92cde47da406f4d4ac6e
Tsugami <yslan3000@gmail.com>
2019-10-20 17:04:13 -0300

    change(listeners): renamed to events

--- src/bot/client/initializers/events.ts
+++ src/bot/client/initializers/events.ts
@@ -10,7 +10,7 @@ interface Listener {
 
 export const required = true;
 export function handle(client: SimplicityClient) {
-  requireDirectory('src/bot/listeners', (error, Event, filename) => {
+  requireDirectory('src/bot/events', (error, Event, filename) => {
     if (error) {
       return client.logger.error(`Não foi possivel carregar o evento: ${filename}`, error);
     }
