e5b442f1653b2d8b576ba6f16257f5eb1355e49a
Tsugami <yslan3000@gmail.com>
2019-11-09 11:35:35 -0300

    move index.js to src and renamed to app.js

---
+++ src/app.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const { SimplicityClient } = require('./');
+const server = require('./api/server');
+
+require('dotenv').config();
+
+const CLIENT_OPTIONS = {
+  fetchAllMembers: true,
+  disableEveryone: true,
+  disabledEvents: ['TYPING_START'],
+  partials: ['MESSAGE', 'CHANNEL'],
+  presence: {
+    activity: {
+      name: '@Simplicity help',
+      type: 'WATCHING',
+    },
+  },
+};
+
+const client = new SimplicityClient(CLIENT_OPTIONS);
+client.login(process.env.DISCORD_TOKEN)
+  .then(() => {
+    server(client);
+    console.log('bot está online!');
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+client
+  .on('shardError', (error, shardID) => console.error(`Shard ${shardID} Error:`, error))
+  .on('invalidated', () => {
+    console.error('The client\'s session is now invalidated.');
+    process.exit(1);
+  });
+
+process
+  .on('unhandledRejection', (error) => console.error('Uncaught Promise Error:', error))
+  .on('uncaughtException', (error) => {
+    const msg = error.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
+    console.error('Uncaught Exception:', msg);
+    process.exit(1);
+  });
