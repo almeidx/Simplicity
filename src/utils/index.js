ccba2442d495cea228d1d32e6b9ee5aca6f667ac
Tsugami <yslan3000@gmail.com>
2019-11-09 12:38:25 -0300

    renamend files and create index.js to search modules

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  Constants: require('./Constants'),
+  FileUtils: require('./FileUtils'),
+  GuildIconGenerator: require('./Canvas/GuildIconGenerator'),
+  Logger: require('./Logger'),
+  LogUtils: require('./LogUtils'),
+  MessageUtils: require('./MessageUtils'),
+  PermissionUtils: require('./PermissionUtils'),
+  RegexEmotes: require('./RegexEmotes'),
+  StarboardHandler: require('./StarboardHandler'),
+  TextUtils: require('./TextUtils'),
+  Utils: require('./Utils'),
+};
