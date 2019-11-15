76dee3431888d8a2d2005017ce703d2d3da60f5a
Tsugami <yslan3000@gmail.com>
2019-11-15 14:46:16 -0300

    fix permissions command

--- src/commands/guild/permissions.js
+++ src/commands/guild/permissions.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const { Command, CommandError, SimplicityEmbed } = require('@structures');
+const { Command, SimplicityEmbed } = require('@structures');
 const { getServerIconURL, checkTick } = require('@utils/Utils');
 const { MANAGER_PERMISSIONS } = require('@utils/Constants');
-const { User } = require('discord.js');
+const { GuildMember } = require('discord.js');
 
 class Permissions extends Command {
   constructor(client) {
@@ -16,60 +16,41 @@ class Permissions extends Command {
       },
     }, [
       {
-        type: 'member',
+        type: 'member|role',
         acceptSelf: true,
         acceptBot: true,
         required: false,
+        missingError: 'commands:permissions.error',
       },
-      {
-        type: 'role',
-        required: false,
-      },
-      [
-        {
-          type: 'role',
-          name: 'role',
-          aliases: ['r'],
-        },
-        {
-          type: 'member',
-          name: 'member',
-          aliases: ['mem', 'user', 'u', 'm'],
-        },
-      ],
     ]);
   }
 
-  run({ author, emoji, guild, send, t }, member, role) {
-    const victim = member || role;
-    console.log(victim);
-    if (!victim) throw new CommandError('commands:permissions.error');
-
-    const isUser = victim instanceof User;
-    const avatar = isUser ? victim.displayAvatarURL() : getServerIconURL(guild);
-    const name = isUser ? victim.tag : victim.name;
-    const title = isUser ? 'commands:permissions.author' : 'commands:permissions.role';
+  run({ author, emoji, guild, member: guildMember, send, t }, victim = guildMember) {
+    const avatar = victim instanceof GuildMember ? victim.user.displayAvatarURL() : getServerIconURL(guild);
+    const name = victim instanceof GuildMember ? victim.user.tag : victim.name;
+    const title = victim instanceof GuildMember ? 'commands:permissions.author' : 'commands:permissions.role';
 
     const embed = new SimplicityEmbed({ author, emoji, t })
       .setAuthor(title, avatar, null, { name });
 
-    const permissions = isUser ? victim.permissions : victim.permissions;
-    for (const p of MANAGER_PERMISSIONS) embed.addField(`permissions:${p}`, checkTick(permissions.has(p)), true);
+    for (const p of MANAGER_PERMISSIONS) {
+      embed.addField(`permissions:${p}`, checkTick(victim.permissions.has(p)), true);
+    }
 
-    const color = this.resoveColor(embed, emoji);
+    const color = this.resolveColor(embed, emoji);
     if (color) embed.setColor(color);
 
     return send(embed);
   }
 
   resolveColor(embed, emoji) {
-    let color;
     const yResult = embed.fields.filter((f) => f.value === emoji('TICK_YES')).length;
     const nResult = embed.fields.filter((f) => f.value === emoji('TICK_NO')).length;
-    if (Math.abs(yResult / embed.fields.length * 100).toFixed(2) >= 70) color = 'GREEN';
-    if (Math.abs(nResult / embed.fields.length * 100).toFixed(2) >= 70) color = 'RED';
-    if (yResult === nResult) color = null;
-    if (color) return color;
+    if (Math.abs(yResult / embed.fields.length * 100).toFixed(2) >= 70) {
+      return 'GREEN';
+    } else if (Math.abs(nResult / embed.fields.length * 100).toFixed(2) >= 70) {
+      return 'RED';
+    }
   }
 }
 
