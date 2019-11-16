269dba90314b297019f19c5d00bea6e96a66cc9a
Tsugami <yslan3000@gmail.com>
2019-11-16 15:27:07 -0300

    fix setnick

--- src/commands/mod/setnick.js
+++ src/commands/mod/setnick.js
@@ -1,24 +1,6 @@
 'use strict';
 
 const { Command, CommandError, SimplicityEmbed } = require('@structures');
-const { MemberParameter, StringParameter } = require('@parameters/');
-
-const MemberParameterOptions = {
-  checkIncludes: false,
-  required: false,
-  canBeGuildOwner: false,
-  canBeAuthor: true,
-  errors: {
-    missingError: 'errors:invalidUser',
-  },
-};
-const StringParameterOptions = {
-  maxLength: 32,
-  default: '',
-  errors: {
-    maxLength: 'commands:setnick.nameTooBig',
-  },
-};
 
 class SetNick extends Command {
   constructor(client) {
@@ -30,32 +12,47 @@ class SetNick extends Command {
         permissions: ['MANAGE_NICKNAMES'],
         clientPermissions: ['MANAGE_NICKNAMES'],
         guildOnly: true,
-        argsRequired: true },
-    });
+        argsRequired: true,
+      },
+    }, [
+      {
+        type: 'member',
+        acceptSelf: true,
+        acceptBot: true,
+        required: true,
+        missingError: 'errors:invalidUser',
+        authorHasHigh: true,
+        clientHasHigh: true,
+      },
+      {
+        type: 'string',
+        required: false,
+        clean: true,
+        maxLength: 32,
+      },
+    ]);
   }
 
-  async run({ args, author, guild, member, send, t }) {
-    const user = await MemberParameter.parse(args.shift(), MemberParameterOptions, {
-      memberAuthor: member,
-      commandName: this.name,
-      author,
-      guild,
-    });
-    const name = await StringParameter.parse(args.join(' '), StringParameterOptions) || '';
+  async run({ t, author, client, channel }, member, nickname = '') {
+    if (member.displayName === nickname) {
+      throw new CommandError('commands:setnick.alreadySet', { member, nickname });
+    } else if (!member.nickname && !nickname) {
+      throw new CommandError('commands:setnick.alreadyReset', { member });
+    }
 
-    if (user.displayName === name) throw new CommandError('commands:setnick.alreadySet', { user, name });
-    else if (!user.nickname && !name) throw new CommandError('commands:setnick.alreadyReset', { user });
+    try {
+      const reason = t('commands:setnick.reason', { author: author.tag });
+      await member.setNickname(nickname, reason);
+    } catch (error) {
+      client.logger.error(error);
+      throw new CommandError('commands:setnick.failed');
+    }
 
-    const renamedTo = (name && t('commands:setnick.success', { user, name })) ||
-      t('commands:setnick.removedNickname', { user });
-    const reason = t('commands:setnick.reason', { author: author.tag });
-
-    const success = await user.setNickname(name, reason).catch(() => null);
-    if (!success) throw new CommandError('commands:setnick.failed');
+    const text = nickname ? 'commands:setnick.success' : 'commands:setnick.removedNickname';
 
     const embed = new SimplicityEmbed({ author })
-      .setDescription(renamedTo);
-    return send(embed);
+      .setDescription(t(text, { member, nickname }));
+    return channel.send(embed);
   }
 }
 
