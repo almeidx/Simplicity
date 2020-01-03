90ab5e39cd576dbc7ce4e20579656517a843b863
Almeida <42935195+Almeeida@users.noreply.github.com>
2020-01-03 16:29:56 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,13 +3,9 @@
 module.exports = {
   apps: [{
     name: 'simplicity',
-    script: 'src/index.js',
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
+    script: 'src/app.js',
     instances: 1,
     autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development',
     },
