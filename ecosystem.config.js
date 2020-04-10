962e27f1277ee84d03665bea725827f7c6d04d45
Almeeida <rodrigofigueire19@gmail.com>
2020-04-10 14:57:53 +0100

    chore: added ecosystem file for pm2 and changed .github stuff to lf as well

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  apps: [{
+    autorestart: true,
+    env: { NODE_ENV: 'production' },
+    instances: 1,
+    name: 'simplicity',
+    script: 'src/index.js',
+  }],
+};
