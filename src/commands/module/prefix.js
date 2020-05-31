db6bc3d8d0a8766cd78b19a41ec73c3e1400b60b
Tsugami <yslan3000@gmail.com>
2020-05-31 18:50:48 -0300

    refactor: remove argument exports

--- src/commands/module/prefix.js
+++ src/commands/module/prefix.js
@@ -1,12 +1,19 @@
 'use strict';
 
-const Arguments = require('@arguments');
 const { Command, SimplicityEmbed, CommandError } = require('@structures');
 
 class Prefix extends Command {
   constructor(client) {
     super(client, 'prefix', {
       aliases: ['setprefix', 'p', 'setp', 'prefixset'],
+      args: [
+        {
+          errors: { maxLength: 'commands:prefix.multiCharacters' },
+          maxLength: 15,
+          minLength: 1,
+          type: 'string',
+        },
+      ],
       category: 'module',
       cooldown: 5000,
       requirements: {
@@ -16,13 +23,7 @@ class Prefix extends Command {
     });
   }
 
-  async run({ message, author, client, guild, prefix: currentPrefix, query, send, t }) {
-    const prefix = await Arguments.string.parse.call({
-      errors: { maxLength: 'commands:prefix.multiCharacters' },
-      maxLength: 15,
-      minLength: 1,
-    }, query, { message, t });
-
+  async run({ author, client, guild, prefix: currentPrefix, send, t }, prefix) {
     if (currentPrefix === prefix) throw new CommandError('commands:prefix.alreadySet', { prefix });
 
     const data = await client.database.guilds.edit(guild.id, { prefix }).catch(() => null);
