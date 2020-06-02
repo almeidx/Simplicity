08922ec7af570f8fd05d9c42e2e353942a1d5ffc
Almeeida <rodrigofigueire19@gmail.com>
2020-06-02 15:07:28 +0100

    refactor: cleaning up

--- src/util/LogUtil.ts
+++ src/util/LogUtil.ts
@@ -1,15 +1,9 @@
 import {
-  Client, Collection, Guild, NewsChannel, TextChannel, GuildManager,
+  Client, Guild, MessageAdditions, MessageOptions, NewsChannel, TextChannel, StringResolvable
 } from 'discord.js';
 
-type TextBasedChannel = NewsChannel | TextChannel
-
-// temporary
-interface SimplicityClient extends Client {
-  database: {
-    guilds: Collection<string, Guild>
-  }
-}
+type TextBasedChannel = NewsChannel | TextChannel;
+type ContentResolvable = StringResolvable | MessageOptions | MessageAdditions;
 
 /**
  * Contains various log related utility methods.
@@ -18,28 +12,30 @@ class LogUtil {
   /**
    * Gets a channel from the database.
    */
-  static async getChannel(client: SimplicityClient, guild: Guild, logName: string) {
+  static async getChannel(client: Client, guild: Guild, logName: string) {
     const guildData = client.database && await client.database.guilds.get(guild.id);
-    const logData = guildData && guildData.logs && guildData.logs[logName];
+    const logData = guildData?.logs?[logName];
     return logData && logData.channelID && guild.channels.cache.get(logData.channelID);
   }
 
   /**
    * Sends a message to a channel using the bot or using a webhook.
    */
-  static async send(channel: TextBasedChannel, ...body) {
-    if (!channel.permissionsFor(channel.guild.me).has('MANAGE_WEBHOOKS')) return channel.send(...body);
+  static async send(channel: TextBasedChannel, body: ContentResolvable) {
+    const me = channel.guild?.me;
+    const permissions = me && channel.permissionsFor(me);
+    if (permissions && !permissions.has('MANAGE_WEBHOOKS')) return channel.send(body);
 
     const webhook = await this.getWebhook(channel);
-    return webhook.send(...body);
+    return webhook.send(body);
   }
 
   /**
    * Resolves a webhook.
    */
   static async getWebhook(channel: TextBasedChannel) {
-    const name = `${channel.client.user.username} Logs`;
-    const avatar = channel.client.user.displayAvatarURL({ dynamic: true, size: 4096 });
+    const name = `${channel.client.user?.username} Logs` || 'Logs';
+    const avatar = channel.client.user?.displayAvatarURL({ dynamic: true, size: 4096 });
 
     const webhooks = await channel.fetchWebhooks();
     let webhook = webhooks.find((wk) => wk.name === name);
