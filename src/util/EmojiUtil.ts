4211cdebca3e46b995580d615e1fa3d9c641a6ce
Tsugami <yslan3000@gmail.com>
2020-06-02 22:44:53 -0300

    feat: add EmojiUtil

---
+++ src/util/EmojiUtil.ts
@@ -0,0 +1,47 @@
+import { Permissions, TextChannel } from 'discord.js';
+import Constants from './Constants';
+
+type CustomEmojiTypes = keyof typeof Constants.EMOJIS_CUSTOM
+type EmojiTypes = keyof typeof Constants.EMOJIS
+
+/**
+ * Contains various emoji utility methods.
+ * @class EmojiUtil
+ */
+class EmojiUtil {
+  static getCustomEmoji(id: CustomEmojiTypes) {
+    return Constants.EMOJIS_CUSTOM[id];
+  }
+
+  static getDefaultEmoji(name: EmojiTypes) {
+    return Constants.EMOJIS[name];
+  }
+
+  static getEmoji(
+    opts: { id?: boolean, channel?: TextChannel },
+    ...emojis: (EmojiTypes | CustomEmojiTypes)[]
+  ) {
+    for (const emoji of emojis) {
+      const custom = EmojiUtil.getCustomEmoji(emoji as CustomEmojiTypes);
+      const { id, channel } = opts;
+      const client = channel?.guild.me;
+      if (
+        client
+        && custom
+        && channel?.permissionsFor(client)?.has(Permissions.FLAGS.USE_EXTERNAL_EMOJIS)
+      ) {
+        const cachedEmoji = channel.client.emojis.cache.get(custom);
+        if (cachedEmoji) return id ? cachedEmoji.id : cachedEmoji.toString();
+      }
+    }
+
+    for (const emoji of emojis) {
+      const normal = EmojiUtil.getDefaultEmoji(emoji as EmojiTypes);
+      if (normal) return normal;
+    }
+
+    return false;
+  }
+}
+
+module.exports = EmojiUtil;
