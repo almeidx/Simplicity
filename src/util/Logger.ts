22d633bdc907f71e7fc90e4f53094d96341e51cb
Tsugami <yslan3000@gmail.com>
2020-06-02 22:29:54 -0300

    feat: add Logger and refactor

---
+++ src/util/Logger.ts
@@ -0,0 +1,91 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import moment from 'moment';
+
+moment.locale('pt-br');
+
+enum Colors {
+  FgBlue = '\x1b[34m',
+  FgCyan = '\x1b[36m',
+  FgGreen = '\x1b[32m',
+  FgMagenta = '\x1b[35m',
+  FgRed = '\x1b[31m',
+  FgWhite = '\x1b[37m',
+  FgYellow = '\x1b[33m',
+};
+
+const resetColor = '\x1b[0m';
+
+const setColor = (color: Colors, text: string) => `${color + text + resetColor}`;
+
+/**
+ * Contains various logger utility methods.
+ * @class Logger
+ */
+export default class Logger {
+  /**
+   * Gets the current timestamp.
+   * @returns {string} The current timestamp formatted.
+   * @private
+   * @readonly
+   */
+  static get timestamp(): string {
+    return setColor(Colors.FgMagenta, moment().format('DD/MM/YYYY HH:mm:ss'));
+  }
+
+  /**
+   * Logs to console.
+   * @param color The color of the log.
+   * @param text The text to log.
+   * @param type The type of log.
+   * @returns {void}
+   * @private
+   */
+  static createLog(color: Colors, text: string, consoleFn: (...args: any) => void): void {
+    return consoleFn(`${Logger.timestamp} ${setColor(color, text)}`);
+  }
+
+  /**
+   * Logs normally.
+   * @param text The text to log
+   */
+  static log(text: string): void {
+    return Logger.createLog(Colors.FgGreen, text, console.log);
+  }
+
+  /**
+   * Logs an error.
+   * @param text The text to log
+   */
+  static error(text: string): void {
+    return Logger.createLog(Colors.FgRed, text, console.error);
+  }
+
+  /**
+   * Logs a warn.
+   * @param text The text to log
+   */
+  static warn(text: string): void {
+    return Logger.createLog(Colors.FgYellow, text, console.warn);
+  }
+
+  // /**
+  //  * Logs a command.
+  //  * @param {Object} object Object with multiple required properties
+  //  * @param {User} object.author The author of the command.
+  //  * @param {TextBasedChannel} object.channel The channel where the command was used.
+  //  * @param {string} object.content The content of the command message.
+  //  * @param {Guild} object.guild The guild where the command was used.
+  //  * @returns {void}
+  //  */
+  // static logCommand({
+  //   guild, channel, author, content,
+  // }) {
+  //   const warn = setColor('FgYellow', '[Command]');
+  //   const g = setColor('FgBlue', guild);
+  //   const c = setColor('FgCyan', `#${channel}`);
+  //   const u = setColor('FgGreen', `@${author}`);
+  //   return console.warn(`${Logger._timestamp} ${warn} ${g} ${c} ${u} ${content}`);
+  // }
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,4 +1,6 @@
 export { default as Constants } from './Constants';
 export { default as EmojiRegex } from './EmojiRegex';
 export { default as ImageUtil } from './ImageUtil';
+export { default as PermissionUtil } from './PermissionUtil';
+export { default as Logger } from './Logger';
 export { default as Util } from './Util';
