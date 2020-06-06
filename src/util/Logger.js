e11c7668bc1c1ca6caa79ab6be29501565605d79
Tsugami <yslan3000@gmail.com>
2020-06-06 20:00:17 -0300

    feat(Logger): add support for multi args

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const { inspect } = require('util');
 const moment = require('moment');
 const LogFile = require('./LogFile');
+const Util = require('./Util');
 moment.locale('pt-br');
 
 const colors = {
@@ -45,15 +47,15 @@ class Logger {
   /**
    * Logs to console.
    * @param {string} color The color of the log.
-   * @param {...*} text The text to log.
    * @param {string} [type='log'] The type of log.
+   * @param {...*} text The text to log.
    * @returns {void}
    * @private
    */
-  static _log(color, text, type = 'log') {
-    console[type](`${Logger.timestampColor} ${setColor(color, text)}`);
+  static _log(color, type, ...texts) {
+    console[type](`${Logger.timestampColor} ${setColor(color, texts)}`);
     if (process.env.NODE_ENV !== 'development') {
-      return LogFile.addInfo(`${Logger.timestamp} ${type.toUpperCase()} - ${text}`);
+      return LogFile.addInfo(`${Logger.timestamp} ${type.toUpperCase()} - ${texts}`);
     }
   }
 
@@ -62,8 +64,8 @@ class Logger {
    * @param {...*} text The text to log
    * @returns {void}
    */
-  static log(text) {
-    return Logger._log('FgGreen', text);
+  static log(...text) {
+    return Logger._log('FgGreen', 'log', ...text);
   }
 
   /**
@@ -71,8 +73,8 @@ class Logger {
    * @param {...*} text The text to log
    * @returns {void}
    */
-  static error(text) {
-    return Logger._log('FgRed', text, 'error');
+  static error(...text) {
+    return Logger._log('FgRed', 'error', ...text);
   }
 
   /**
@@ -80,29 +82,55 @@ class Logger {
    * @param {...*} text The text to log
    * @returns {void}
    */
-  static warn(text) {
-    return Logger._log('FgYellow', text, 'warn');
+  static warn(...text) {
+    return Logger._log('FgYellow', 'warn', ...text);
   }
 
   /**
    * Logs a command.
-   * @param {Object} object Object with multiple required properties
-   * @param {User} object.author The author of the command.
-   * @param {TextBasedChannel} object.channel The channel where the command was used.
-   * @param {string} object.content The content of the command message.
-   * @param {Guild} object.guild The guild where the command was used.
+   * @param {Message} message Message discordjs
    * @returns {void}
    */
-  static logCommand({ guild, channel, author, content }) {
+  static logCommand(message) {
+    const { guild, channel, author, content } = message;
     if (process.env.NODE_ENV === 'development') {
       const warn = setColor('FgYellow', '[Command]');
-      const g = setColor('FgBlue', guild);
-      const c = setColor('FgCyan', `#${channel}`);
-      const u = setColor('FgGreen', `@${author}`);
-      return console.warn(`${Logger._timestamp} ${warn} ${g} ${c} ${u} ${content}`);
+      const g = setColor('FgBlue', guild ? guild.name : 'DM');
+      const c = setColor('FgCyan', `#${channel.name}`);
+      const u = setColor('FgGreen', `@${author.username}`);
+      return console.warn(`${Logger.timestampColor} ${warn} ${g} ${c} ${u} ${content}`);
     }
     return LogFile.addInfo(
-      `${Logger.timestamp} COMMAND_USE - GUILD_ID: ${guild.id} USER_ID: ${author.id} CONTENT: ${content}`);
+      `${Logger.timestamp} COMMAND_USE - GUILD_ID: ${guild.id} USER_ID: ${author.id} ${Logger.resolveContent(message)}`,
+    );
+  }
+
+  /** * Resolves the content of a message.
+   * @param {Message} message The message from which it's content will be resolved.
+   * @returns {string} The content.
+   * */
+  static resolveContent(message) {
+    const { isEmpty } = Util;
+    if (message.content && isEmpty(message.embeds) && isEmpty(message.attachments)) {
+      return message.content;
+    }
+    let content = '';
+    if (message.content) {
+      content += `Content: ${message.content}\n`;
+    }
+    if (!isEmpty(message.embeds)) {
+      let i = 1;
+      for (const embed of message.embeds.values()) {
+        content += `Embed ${i}: ${inspect(embed.toJSON(), { depth: 3 })}\n`; i++;
+      }
+    }
+    if (!isEmpty(message.attachments)) {
+      let i = 1;
+      for (const attachment of message.attachments.values()) {
+        content += `Attachment ${i}: ${attachment.url}`; i++;
+      }
+    }
+    return content;
   }
 }
 
