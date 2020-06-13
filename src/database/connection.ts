eaa421c2134a48bc996ffc171d685a6dceeb2296
Almeeida <rodrigofigueire19@gmail.com>
2020-06-13 14:47:56 +0100

    fix(database/connection): deprecated

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,7 +2,11 @@
 import mongoose, { Mongoose } from 'mongoose';
 
 const connect = (uri = String(process.env.MONGO_URI)): Promise<Mongoose> => {
-  return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  return mongoose.connect(uri, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connect;
