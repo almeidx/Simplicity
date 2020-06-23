d7eaac0b8ceb2ad04d593176b6a833e9b9a52286
Yslan Ramos <yslan3000@gmail.com>
2020-06-23 15:40:34 -0300

    featLogger): add figures and filename

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,23 +1,36 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable no-console */
 
+// eslint-disable-next-line max-classes-per-file
 import { format } from 'date-fns';
 import { Message, GuildChannel } from 'discord.js';
+import path from 'path';
+import figures from 'figures';
 
 enum Colors {
   FgBlue = '\x1b[34m',
   FgCyan = '\x1b[36m',
   FgGreen = '\x1b[32m',
-  FgMagenta = '\x1b[35m',
+  FgLightMagenta = '\x1b[95m',
   FgRed = '\x1b[31m',
   FgWhite = '\x1b[37m',
   FgYellow = '\x1b[33m',
+  fgDarkGray = '\x1b[90m'
 }
 
-const resetColor = '\x1b[0m';
+class TerminalFormatter {
+  static bold(text: string): string {
+    return `\x1b[1m${text}\x1b[0m`;
+  }
 
-const setColor = (color: Colors, message: string) => `${color + message + resetColor}`;
+  static underlined(text: string): string {
+    return `\x1b[4m${text}\x1b[0m`;
+  }
 
+  static color(color: Colors, text: string): string {
+    return `${color + text}\x1b[0m`;
+  }
+}
 /**
  * Contains various logger utility methods
  */
@@ -26,8 +39,12 @@ export default class Logger {
    * Gets the current timestamp
    * @returns The current timestamp formatted
    */
-  static get timestamp(): string {
-    return setColor(Colors.FgMagenta, format(Date.now(), 'dd/mm/yyyy hh:mm:ss'));
+  static formatTimestamp(timestamp = Date.now()): string {
+    return TerminalFormatter.color(Colors.fgDarkGray, format(timestamp, 'd LLL HH:mm:ss'));
+  }
+
+  static formatFilename(filename: string): string {
+    return `${filename.replace(/.js|.ts|.json/g, '')}`;
   }
 
   /**
@@ -37,36 +54,63 @@ export default class Logger {
    * @param type The type of log
    */
   static createLog(
-    consoleFn: (...args: any) => void,
+    logType: 'warn' | 'log' | 'error',
+    figure: string,
     color: Colors,
-    message: any,
-    optionalParams: any[] = [],
+    messages: string[],
   ): void {
-    return consoleFn(`${Logger.timestamp} ${setColor(color, message)}`, ...optionalParams);
+    const formattedfigure = TerminalFormatter.color(color, figure);
+    const filename = Logger.formatFilename(Logger.getFilename());
+    const timestamp = Logger.formatTimestamp(Date.now());
+    const message = TerminalFormatter.color(color, messages.join(', '));
+
+    return console[logType](`${formattedfigure} ${timestamp} ${filename} ${message}`);
+  }
+
+  static getFilename(): string {
+    const _ = Error.prepareStackTrace;
+    Error.prepareStackTrace = (error, stack) => stack;
+    const { stack } = new Error();
+    Error.prepareStackTrace = _;
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const callers: string[] = stack.map((x) => x.getFileName());
+
+    const firstExternalFilePath = callers.find((x: string) => x !== callers[0]);
+
+    return firstExternalFilePath ? path.basename(firstExternalFilePath) : 'anonymous';
   }
 
   /**
    * Logs normally
    * @param message The message to log
    */
-  static log(message: any, ...optionalParams: any[]): void {
-    return Logger.createLog(console.log, Colors.FgGreen, message, optionalParams);
+  static log(...messages: string[]): void {
+    return Logger.createLog('log', figures.arrowRight, Colors.FgGreen, messages);
+  }
+
+  static success(...messages: string[]): void {
+    return Logger.createLog('log', figures.tick, Colors.FgGreen, messages);
+  }
+
+  static info(...messages: string[]): void {
+    return Logger.createLog('log', figures.info, Colors.FgGreen, messages);
   }
 
   /**
    * Logs an error
    * @param message The message to log
    */
-  static error(message: any, ...optionalParams: any[]): void {
-    return Logger.createLog(console.error, Colors.FgRed, message, optionalParams);
+  static error(...messages: string[]): void {
+    return Logger.createLog('error', figures.cross, Colors.FgRed, messages);
   }
 
   /**
    * Logs a warn
    * @param message The message to log
    */
-  static warn(message: any, ...optionalParams: any[]): void {
-    return Logger.createLog(console.warn, Colors.FgYellow, message, optionalParams);
+  static warn(...messages: string[]): void {
+    return Logger.createLog('warn', figures.warning, Colors.FgYellow, messages);
   }
 
   /**
@@ -77,12 +121,13 @@ export default class Logger {
   static logCommand({
     guild, channel, author, cleanContent,
   }: Message) {
-    let message = ` ${setColor(Colors.FgYellow, '[Command]')}`;
+    let message = '';
     if (guild) {
-      message += ` ${setColor(Colors.FgBlue, guild.name)}`;
-      message += ` ${setColor(Colors.FgCyan, `#${(channel as GuildChannel).name}`)}`;
+      message += `${TerminalFormatter.color(Colors.FgBlue, guild.name)} `;
+      message += `${TerminalFormatter.color(Colors.FgCyan, `#${(channel as GuildChannel).name}`)} `;
     }
-    message += ` ${setColor(Colors.FgGreen, `@${author.username}`)}`;
-    return console.warn(`${Logger.timestamp} ${message} ${cleanContent}`);
+    message += `${TerminalFormatter.color(Colors.FgGreen, `@${author.username}`)} `;
+    const figure = TerminalFormatter.color(Colors.FgLightMagenta, figures.play);
+    return console.warn(`${figure} ${Logger.formatTimestamp()} ${message}${cleanContent}`);
   }
 }
